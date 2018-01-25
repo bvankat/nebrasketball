10c61a2caaa27b88654e98a88a41a8dba9fd6a67
Ben Vankat <bvankat@gmail.com>
2018-01-24 23:09:29 -0600

    latest update

--- data.js
+++ data.js
@@ -1,1 +1,55 @@
-{"trank": {"trank": "62", "sagarin": "69", "current_wl": "15-8", "proj": "20-11 (11-7)", "current_cwl": "6-4", "kpi": "70"}, "espn": {"rpi": "64", "sos": "19", "seven_day_change": "14", "proj_win_loss_conf": "9.8-8.2", "bpi": "74", "proj_win_loss_overall": "18.8-12.2", "sor": "48", "proj_tourney_seed": "12"}, "warrennolan": {"group4": "8-0", "sos": "86", "group1": "0-5", "cwl": "(9-9)", "group3": "4-0", "group2": "3-3", "rpi": "61", "wl": "18-13"}, "ncaa": {"previous_rpi_rank": "67", "rpi_rank": "70"}, "kenpom": {"rating": "62", "sos": "45"}, "time": {"time": "Jan 24, 10:24 PM"}, "teamrankings": {"projected_losses": "12.6", "projected_wins": "18.4", "rank": "81", "sos": "53", "most_likely_seed": "#12 (1.5%)", "make_tournament": "3.7%"}, "lrmc": {"rank": "48"}}
+{
+    "bracketproject": {
+        "matrix_count": "1"
+    },
+    "espn": {
+        "bpi": "74",
+        "proj_tourney_seed": "12",
+        "proj_win_loss_conf": "9.8-8.2",
+        "proj_win_loss_overall": "18.8-12.2",
+        "rpi": "64",
+        "seven_day_change": "14",
+        "sor": "48",
+        "sos": "19"
+    },
+    "kenpom": {
+        "rating": "62",
+        "sos": "45"
+    },
+    "lrmc": {
+        "rank": "48"
+    },
+    "ncaa": {
+        "previous_rpi_rank": "67",
+        "rpi_rank": "70"
+    },
+    "teamrankings": {
+        "make_tournament": "3.7%",
+        "most_likely_seed": "#12 (1.5%)",
+        "projected_losses": "12.6",
+        "projected_wins": "18.4",
+        "rank": "81",
+        "sos": "53"
+    },
+    "time": {
+        "time": "Jan 24, 11:09 PM"
+    },
+    "trank": {
+        "current_cwl": "6-4",
+        "current_wl": "15-8",
+        "kpi": "70",
+        "proj": "20-11 (11-7)",
+        "sagarin": "69",
+        "trank": "62"
+    },
+    "warrennolan": {
+        "cwl": "(9-9)",
+        "group1": "0-5",
+        "group2": "3-3",
+        "group3": "5-0",
+        "group4": "7-0",
+        "rpi": "60",
+        "sos": "83",
+        "wl": "18-13"
+    }
+}
