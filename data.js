215ae4c7226c304fde400a461fa27f2347225f1e
Ben Vankat <bvankat@gmail.com>
2018-01-28 08:20:52 -0600

    latest update

--- data.js
+++ data.js
@@ -8,13 +8,13 @@
         "proj_win_loss_conf": "10.6-7.4",
         "proj_win_loss_overall": "19.6-11.4",
         "rpi": "63",
-        "seven_day_change": "0",
+        "seven_day_change": "1",
         "sor": "45",
         "sos": "21"
     },
     "kenpom": {
-        "rating": "67",
-        "sos": "50"
+        "rating": "60",
+        "sos": "59"
     },
     "lrmc": {
         "rank": "45"
@@ -27,32 +27,32 @@
         "rpi_rank": "64"
     },
     "teamrankings": {
-        "make_tournament": "5.6%",
-        "most_likely_seed": "#12 (2.2%)",
-        "projected_losses": "12.0",
-        "projected_wins": "19.0",
-        "rank": "77",
-        "sos": "52"
+        "make_tournament": "7.1%",
+        "most_likely_seed": "#12 (2.8%)",
+        "projected_losses": "11.6",
+        "projected_wins": "19.4",
+        "rank": "75",
+        "sos": "57"
     },
     "time": {
-        "time": "Jan 27,  5:01 PM"
+        "time": "Jan 28,  8:20 AM"
     },
     "trank": {
-        "current_cwl": "6-4",
-        "current_wl": "15-8",
+        "current_cwl": "7-4",
+        "current_wl": "16-8",
         "kpi": "70",
         "proj": "20-11 (11-7)",
         "sagarin": "67",
         "trank": "61"
     },
     "warrennolan": {
-        "cwl": "(8-9)",
+        "cwl": "(9-9)",
         "group1": "0-5",
         "group2": "3-3",
         "group3": "5-0",
-        "group4": "7-0",
-        "rpi": "66",
-        "sos": "86",
-        "wl": "17-13"
+        "group4": "8-0",
+        "rpi": "65",
+        "sos": "87",
+        "wl": "18-13"
     }
 }
