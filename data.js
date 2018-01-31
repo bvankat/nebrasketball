4867bd27c00f33c5fd70944081d5a5017e749bee
Ben Vankat <bvankat@gmail.com>
2018-01-31 10:01:14 -0600

    latest update

--- data.js
+++ data.js
@@ -19,22 +19,22 @@
         "rank": "35"
     },
     "massey": {
-        "composite": "61"
+        "composite": "59"
     },
     "ncaa": {
-        "previous_rpi_rank": "65",
-        "rpi_rank": "57"
+        "previous_rpi_rank": "57",
+        "rpi_rank": "58"
     },
     "teamrankings": {
-        "make_tournament": "14.7%",
-        "most_likely_seed": "#12 (5.5%)",
+        "make_tournament": "15.1%",
+        "most_likely_seed": "#12 (5.7%)",
         "projected_losses": "10.8",
         "projected_wins": "20.2",
         "rank": "69",
         "sos": "53"
     },
     "time": {
-        "time": "Jan 31,  3:01 AM"
+        "time": "Jan 31, 10:01 AM"
     },
     "trank": {
         "current_cwl": "8-4",
