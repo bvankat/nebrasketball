4a7233b2b0b4e94b7201554a54b96e2b576ce9d7
Ben Vankat <bvankat@gmail.com>
2018-02-07 22:01:18 -0600

    latest update

--- data.js
+++ data.js
@@ -12,7 +12,7 @@
         "sos": "39"
     },
     "kenpom": {
-        "rating": "57",
+        "rating": "58",
         "sos": "52"
     },
     "lrmc": {
@@ -34,21 +34,9 @@
         "sos": "51"
     },
     "time": {
-        "time": "Feb 07,  3:01 PM"
+        "time": "Feb 07, 10:01 PM"
     },
     "todays_games": [
-        {
-            "next_opponent": "vs LSU",
-            "rpi": "49",
-            "team": "Florida",
-            "time": " 5:30 PM"
-        },
-        {
-            "next_opponent": "vs Kansas St.",
-            "rpi": "39",
-            "team": "Texas",
-            "time": " 7:00 PM"
-        },
         {
             "next_opponent": "@ Va. Tech",
             "rpi": "59",
@@ -66,30 +54,6 @@
             "rpi": "68",
             "team": "Va. Tech",
             "time": " 8:00 PM"
-        },
-        {
-            "next_opponent": "@ Vanderbilt",
-            "rpi": "60",
-            "team": "Georgia",
-            "time": " 7:30 PM"
-        },
-        {
-            "next_opponent": "@ Texas",
-            "rpi": "70",
-            "team": "Kansas St.",
-            "time": " 7:00 PM"
-        },
-        {
-            "next_opponent": "@ Penn St.",
-            "rpi": "57",
-            "team": "Maryland",
-            "time": " 5:30 PM"
-        },
-        {
-            "next_opponent": "vs Saint Louis",
-            "rpi": "48",
-            "team": "St. Bona.",
-            "time": " 6:00 PM"
         }
     ],
     "trank": {
@@ -102,12 +66,12 @@
     },
     "warrennolan": {
         "cwl": "(13-5)",
-        "group1": "0-5",
-        "group2": "3-3",
+        "group1": "0-6",
+        "group2": "3-2",
         "group3": "8-0",
         "group4": "7-0",
-        "rpi": "54",
-        "sos": "93",
+        "rpi": "55",
+        "sos": "94",
         "wl": "22-9"
     }
 }
