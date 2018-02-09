8ae454cc4bb28744118cfad58cd34a714111ee91
Ben Vankat <bvankat@gmail.com>
2018-02-09 00:01:50 -0600

    latest update

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 {
     "bracketproject": {
-        "matrix_count": "8"
+        "matrix_count": "9"
     },
     "espn": {
         "bpi": "65",
@@ -12,7 +12,7 @@
         "sos": "39"
     },
     "kenpom": {
-        "rating": "57",
+        "rating": "56",
         "sos": "52"
     },
     "lrmc": {
@@ -34,64 +34,9 @@
         "sos": "50"
     },
     "time": {
-        "time": "Feb 08,  6:15 PM"
+        "time": "Feb 09, 12:01 AM"
     },
-    "todays_games": [
-        {
-            "next_opponent": "vs SMU",
-            "rpi": "38",
-            "team": "Houston",
-            "time": " 8:00 PM"
-        },
-        {
-            "next_opponent": "vs Georgia Tech",
-            "rpi": "43",
-            "team": "Louisville",
-            "time": " 6:00 PM"
-        },
-        {
-            "next_opponent": "vs Rice",
-            "rpi": "22",
-            "team": "Middle Tenn.",
-            "time": " 6:30 PM"
-        },
-        {
-            "next_opponent": "vs Cal-Baker.",
-            "rpi": "45",
-            "team": "New Mex. St.",
-            "time": " 8:00 PM"
-        },
-        {
-            "next_opponent": "@ Houston",
-            "rpi": "71",
-            "team": "SMU",
-            "time": " 8:00 PM"
-        },
-        {
-            "next_opponent": "@ Arizona",
-            "rpi": "57",
-            "team": "UCLA",
-            "time": " 9:00 PM"
-        },
-        {
-            "next_opponent": "@ Arizona St.",
-            "rpi": "44",
-            "team": "USC",
-            "time": "10:00 PM"
-        },
-        {
-            "next_opponent": "vs Stanford",
-            "rpi": "58",
-            "team": "Utah",
-            "time": " 7:00 PM"
-        },
-        {
-            "next_opponent": "vs FAU",
-            "rpi": "51",
-            "team": "Western Ky.",
-            "time": " 7:30 PM"
-        }
-    ],
+    "todays_games": [],
     "trank": {
         "current_cwl": "9-4",
         "current_wl": "18-8",
