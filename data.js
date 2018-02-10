8d094277ce30aa5f7dea92991be5496dff35dcea
Ben Vankat <bvankat@gmail.com>
2018-02-10 00:05:56 -0600

    latest update

--- data.js
+++ data.js
@@ -13,7 +13,7 @@
     },
     "kenpom": {
         "rating": "56",
-        "sos": "52"
+        "sos": "54"
     },
     "lrmc": {
         "rank": "43"
@@ -34,9 +34,148 @@
         "sos": "50"
     },
     "time": {
-        "time": "Feb 09,  5:19 PM"
+        "time": "Feb 10, 12:05 AM"
     },
-    "todays_games": [],
+    "todays_games": [
+        {
+            "next_opponent": "vs Miss. State",
+            "rpi": "30",
+            "team": "Missouri",
+            "time": " 1:00 PM"
+        },
+        {
+            "next_opponent": "vs Kentucky",
+            "rpi": "18",
+            "team": "Texas A&M",
+            "time": " 7:15 PM"
+        },
+        {
+            "next_opponent": "@ Oregon St.",
+            "rpi": "39",
+            "team": "Washington",
+            "time": " 9:00 PM"
+        },
+        {
+            "next_opponent": "vs Tennessee",
+            "rpi": "42",
+            "team": "Alabama",
+            "time": " 5:00 PM"
+        },
+        {
+            "next_opponent": "vs Vanderbilt",
+            "rpi": "35",
+            "team": "Arkansas",
+            "time": " 7:30 PM"
+        },
+        {
+            "next_opponent": "@ S. Carolina",
+            "rpi": "50",
+            "team": "Florida",
+            "time": "11:00 AM"
+        },
+        {
+            "next_opponent": "vs Texas Tech",
+            "rpi": "59",
+            "team": "Kansas St.",
+            "time": " 7:00 PM"
+        },
+        {
+            "next_opponent": "vs North Texas",
+            "rpi": "29",
+            "team": "Middle Tenn.",
+            "time": " 5:00 PM"
+        },
+        {
+            "next_opponent": "vs N. Carolina",
+            "rpi": "65",
+            "team": "NC State",
+            "time": " 1:00 PM"
+        },
+        {
+            "next_opponent": "vs San Diego St",
+            "rpi": "14",
+            "team": "Nevada",
+            "time": " 7:00 PM"
+        },
+        {
+            "next_opponent": "vs Grand Canyon",
+            "rpi": "48",
+            "team": "New Mex. St.",
+            "time": " 8:00 PM"
+        },
+        {
+            "next_opponent": "vs Texas",
+            "rpi": "31",
+            "team": "TCU",
+            "time": " 1:00 PM"
+        },
+        {
+            "next_opponent": "@ TCU",
+            "rpi": "46",
+            "team": "Texas",
+            "time": " 1:00 PM"
+        },
+        {
+            "next_opponent": "@ Arizona St.",
+            "rpi": "51",
+            "team": "UCLA",
+            "time": " 6:00 PM"
+        },
+        {
+            "next_opponent": "@ Arizona",
+            "rpi": "44",
+            "team": "USC",
+            "time": " 9:15 PM"
+        },
+        {
+            "next_opponent": "@ Virginia",
+            "rpi": "67",
+            "team": "Va. Tech",
+            "time": " 5:15 PM"
+        },
+        {
+            "next_opponent": "@ Utah St.",
+            "rpi": "32",
+            "team": "Boise St.",
+            "time": " 8:00 PM"
+        },
+        {
+            "next_opponent": "@ St. John's",
+            "rpi": "57",
+            "team": "Marquette",
+            "time": "11:00 AM"
+        },
+        {
+            "next_opponent": "@ Missouri",
+            "rpi": "55",
+            "team": "Miss. State",
+            "time": " 1:00 PM"
+        },
+        {
+            "next_opponent": "vs Rutgers",
+            "rpi": "54",
+            "team": "Nebraska",
+            "time": " 3:00 PM"
+        },
+        {
+            "next_opponent": "vs Richmond",
+            "rpi": "47",
+            "team": "St. Bona.",
+            "time": " 3:00 PM"
+        },
+        {
+            "next_opponent": "vs California",
+            "rpi": "58",
+            "team": "Utah",
+            "time": " 8:30 PM"
+        },
+        {
+            "next_opponent": "vs FIU",
+            "rpi": "56",
+            "team": "Western Ky.",
+            "time": " 6:00 PM"
+        }
+    ],
     "trank": {
         "current_cwl": "9-4",
         "current_wl": "18-8",
@@ -51,8 +190,8 @@
         "group2": "3-2",
         "group3": "7-0",
         "group4": "8-0",
-        "rpi": "54",
-        "sos": "93",
+        "rpi": "57",
+        "sos": "95",
         "wl": "21-9"
     }
 }
