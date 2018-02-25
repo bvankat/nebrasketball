bb45c48b641517ae8d5030023fb14371a33c4961
Ben Vankat <bvankat@gmail.com>
2018-02-25 01:11:55 -0600

    latest update

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 {
     "bracketproject": {
-        "matrix_count": "2"
+        "matrix_count": "3"
     },
     "espn": {
-        "bpi": "61",
+        "bpi": "62",
         "proj_tourney_seed": "9",
         "proj_win_loss_conf": "12.5-5.5",
         "proj_win_loss_overall": "21.5-9.5",
-        "seven_day_change": "-3",
-        "sor": "34",
-        "sos": "59"
+        "seven_day_change": "-4",
+        "sor": "35",
+        "sos": "61"
     },
     "kenpom": {
-        "rating": "55",
+        "rating": "59",
         "sos": "64"
     },
     "lrmc": {
@@ -22,28 +22,47 @@
         "composite": "59"
     },
     "ncaa": {
-        "previous_rpi_rank": "59",
-        "rpi_rank": "60"
+        "previous_rpi_rank": "60",
+        "rpi_rank": "61"
     },
     "teamrankings": {
-        "make_tournament": "41.9%",
-        "most_likely_seed": "#12 (15.1%)",
+        "make_tournament": "41.4%",
+        "most_likely_seed": "#12 (15.4%)",
         "projected_losses": "9.5",
         "projected_wins": "21.5",
         "rank": "69",
         "sos": "61"
     },
     "time": {
-        "time": "Feb 23,  4:16 PM"
+        "time": "Feb 25,  1:11 AM"
     },
-    "todays_games": [],
+    "todays_games": [
+        {
+            "next_opponent": "vs Florida St.",
+            "rpi": "56",
+            "team": "NC State",
+            "time": " 5:00 PM"
+        },
+        {
+            "next_opponent": "vs Penn St.",
+            "rpi": "61",
+            "team": "Nebraska",
+            "time": " 4:15 PM"
+        },
+        {
+            "next_opponent": "@ Colorado",
+            "rpi": "50",
+            "team": "UCLA",
+            "time": " 3:00 PM"
+        }
+    ],
     "trank": {
         "current_cwl": "12-5",
         "current_wl": "21-9",
         "kpi": "63",
         "proj": "22-9 (13-5)",
         "sagarin": "61",
-        "trank": "57"
+        "trank": "58"
     },
     "warrennolan": {
         "cwl": "(12-6)",
@@ -51,8 +70,8 @@
         "group2": "2-3",
         "group3": "7-1",
         "group4": "11-0",
-        "rpi": "60",
-        "sos": "103",
+        "rpi": "61",
+        "sos": "105",
         "wl": "21-10"
     }
 }
