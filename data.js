8f38fe7193b1fb352c111f73a67b8afbf0682cb1
Ben Vankat <bvankat@gmail.com>
2018-03-03 21:45:36 -0600

    latest update

--- data.js
+++ data.js
@@ -3,20 +3,20 @@
         "matrix_count": "7"
     },
     "espn": {
-        "bpi": "58",
+        "bpi": "62",
         "proj_tourney_seed": "9",
         "proj_win_loss_conf": "13.0-5.0",
-        "proj_win_loss_overall": "22.2-9.8",
-        "seven_day_change": "3",
-        "sor": "33",
-        "sos": "60"
+        "proj_win_loss_overall": "22.0-10.0",
+        "seven_day_change": "0",
+        "sor": "35",
+        "sos": "58"
     },
     "kenpom": {
         "rating": "56",
-        "sos": "59"
+        "sos": "61"
     },
     "lrmc": {
-        "rank": "46"
+        "rank": "48"
     },
     "massey": {
         "composite": "52"
@@ -34,161 +34,15 @@
         "sos": "59"
     },
     "time": {
-        "time": "Mar 03,  9:30 AM"
+        "time": "Mar 03,  9:45 PM"
     },
-    "todays_games": [
-        {
-            "next_opponent": "vs Stanford",
-            "rpi": "41",
-            "team": "Arizona St.",
-            "time": " 1:30 PM"
-        },
-        {
-            "next_opponent": "@ Seton Hall",
-            "rpi": "39",
-            "team": "Butler",
-            "time": " 7:00 PM"
-        },
-        {
-            "next_opponent": "vs Kentucky",
-            "rpi": "43",
-            "team": "Florida",
-            "time": "11:00 AM"
-        },
-        {
-            "next_opponent": "vs Boston College",
-            "rpi": "46",
-            "team": "Florida St.",
-            "time": " 1:00 PM"
-        },
-        {
-            "next_opponent": "vs Marshall",
-            "rpi": "20",
-            "team": "Middle Tenn.",
-            "time": " 6:00 PM"
-        },
-        {
-            "next_opponent": "vs Arkansas",
-            "rpi": "38",
-            "team": "Missouri",
-            "time": " 5:00 PM"
-        },
-        {
-            "next_opponent": "vs Louisville",
-            "rpi": "59",
-            "team": "NC State",
-            "time": " 5:00 PM"
-        },
-        {
-            "next_opponent": "vs Pepperdine",
-            "rpi": "36",
-            "team": "St. Mary's",
-            "time": "11:00 PM"
-        },
-        {
-            "next_opponent": "vs Alabama",
-            "rpi": "23",
-            "team": "Texas A&M",
-            "time": " 1:00 PM"
-        },
-        {
-            "next_opponent": "@ Texas A&M",
-            "rpi": "57",
-            "team": "Alabama",
-            "time": " 1:00 PM"
-        },
-        {
-            "next_opponent": "vs Baylor",
-            "rpi": "63",
-            "team": "Kansas St.",
-            "time": " 1:00 PM"
-        },
-        {
-            "next_opponent": "@ NC State",
-            "rpi": "39",
-            "team": "Louisville",
-            "time": " 5:00 PM"
-        },
-        {
-            "next_opponent": "",
-            "rpi": "37",
-            "team": "Oklahoma",
-            "time": " 5:00 PM"
-        },
-        {
-            "next_opponent": "vs St. John's",
-            "rpi": "42",
-            "team": "Providence",
-            "time": "11:00 AM"
-        },
-        {
-            "next_opponent": "@ Saint Louis",
-            "rpi": "24",
-            "team": "St. Bona.",
-            "time": " 7:00 PM"
-        },
-        {
-            "next_opponent": "vs Clemson",
-            "rpi": "49",
-            "team": "Syracuse",
-            "time": " 1:00 PM"
-        },
-        {
-            "next_opponent": "vs W. Virginia",
-            "rpi": "58",
-            "team": "Texas",
-            "time": "11:00 AM"
-        },
-        {
-            "next_opponent": "@ USC",
-            "rpi": "55",
-            "team": "UCLA",
-            "time": " 9:15 PM"
-        },
-        {
-            "next_opponent": "vs UCLA",
-            "rpi": "32",
-            "team": "USC",
-            "time": " 9:15 PM"
-        },
-        {
-            "next_opponent": "vs Colorado",
-            "rpi": "54",
-            "team": "Utah",
-            "time": " 6:00 PM"
-        },
-        {
-            "next_opponent": "vs Oregon",
-            "rpi": "44",
-            "team": "Washington",
-            "time": " 3:30 PM"
-        },
-        {
-            "next_opponent": "@ Kansas St.",
-            "rpi": "55",
-            "team": "Baylor",
-            "time": " 1:00 PM"
-        },
-        {
-            "next_opponent": "vs Creighton",
-            "rpi": "64",
-            "team": "Marquette",
-            "time": " 1:30 PM"
-        },
-        {
-            "next_opponent": "@ LSU",
-            "rpi": "65",
-            "team": "Miss. State",
-            "time": "12:00 PM"
-        }
-    ],
     "trank": {
         "current_cwl": "13-5",
         "current_wl": "22-10",
         "kpi": "64",
-        "proj": "Fri 03-02\nN\n45\n13 (I)Michigan Michigan -4.6, 68-64 (67%)\nL, 77-58 \n22-10\n\n39\n-9.7",
-        "sagarin": "55",
-        "trank": "55"
+        "proj": "Fri 03-02\nN\n45\n8 (I)Michigan Michigan -4.6, 68-64 (67%)\nL, 77-58 \n22-10\n\n41\n-9.7",
+        "sagarin": "60",
+        "trank": "57"
     },
     "warrennolan": {
         "cwl": "(13-5)",
@@ -196,8 +50,8 @@
         "group2": "3-2",
         "group3": "8-1",
         "group4": "10-0",
-        "rpi": "61",
-        "sos": "92",
+        "rpi": "64",
+        "sos": "97",
         "wl": "22-10"
     }
 }
