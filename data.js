a7f869f8a7ffae567e15e65927b44685d16bb523
Ben Vankat <bvankat@gmail.com>
2018-03-09 13:02:01 -0600

    latest update

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 {
     "bracketproject": {
-        "matrix_count": "4"
+        "matrix_count": "3"
     },
     "espn": {
         "bpi": "62",
@@ -16,32 +16,32 @@
         "sos": "62"
     },
     "lrmc": {
-        "rank": "53"
+        "rank": "58"
     },
     "massey": {
         "composite": "55"
     },
     "ncaa": {
         "previous_rpi_rank": "57",
-        "rpi_rank": "57"
+        "rpi_rank": "59"
     },
     "teamrankings": {
-        "make_tournament": "84.3%",
-        "most_likely_seed": "#11 (26.2%)",
+        "make_tournament": "82.8%",
+        "most_likely_seed": "#11 (25.4%)",
         "projected_losses": "10.0",
         "projected_wins": "22.0",
-        "rank": "68",
+        "rank": "67",
         "sos": "58"
     },
     "time": {
-        "time": "Mar 09,  2:01 AM"
+        "time": "Mar 09,  1:02 PM"
     },
     "trank": {
         "current_cwl": "13-5",
         "current_wl": "22-10",
-        "kpi": "64",
+        "kpi": "63",
         "proj": "Fri 03-02\nN\n45\n8 (I)Michigan Michigan -4.6, 68-64 (67%)\nL, 77-58 \n22-10\n\n43\n-9.7",
-        "sagarin": "62",
+        "sagarin": "60",
         "trank": "54"
     },
     "warrennolan": {
