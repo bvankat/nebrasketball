7ad34f7c301535c5a85b7043375a554b9449ed7f
Ben Vankat <bvankat@gmail.com>
2018-11-20 01:44:57 -0600

    latest update

--- data.js
+++ data.js
@@ -1,57 +1,38 @@
 {
     "bracketproject": {
-        "matrix_count": "3"
+        "matrix_count": "40"
     },
     "espn": {
-        "bpi": "63",
-        "proj_tourney_seed": "9",
-        "proj_win_loss_conf": "13.0-5.0",
-        "proj_win_loss_overall": "22.0-10.0",
-        "seven_day_change": "1",
-        "sor": "35",
-        "sos": "59"
+        "bpi": "40",
+        "proj_tourney_seed": "14",
+        "proj_win_loss_conf": "10.3-9.7",
+        "proj_win_loss_overall": "18.6-11.4",
+        "seven_day_change": "0",
+        "sor": "55",
+        "sos": "302"
     },
     "kenpom": {
-        "rating": "57",
-        "sos": "63"
-    },
-    "lrmc": {
-        "rank": "61"
-    },
-    "massey": {
-        "composite": "55"
-    },
-    "ncaa": {
-        "previous_rpi_rank": "57",
-        "rpi_rank": "55"
+        "rating": "23",
+        "sos": "302"
     },
     "teamrankings": {
-        "make_tournament": "77.4%",
-        "most_likely_seed": "#11 (22.9%)",
-        "projected_losses": "10.0",
-        "projected_wins": "22.0",
-        "rank": "66",
-        "sos": "61"
+        "make_tournament": "88.0%",
+        "most_likely_seed": "#2 (18.3%)",
+        "projected_losses": "6.9",
+        "projected_wins": "22.1",
+        "rank": "6"
     },
     "time": {
-        "time": "Mar 11,  5:01 PM"
-    },
-    "trank": {
-        "current_cwl": "13-5",
-        "current_wl": "22-10",
-        "kpi": "64",
-        "proj": "Fri 03-02\nN\n45\n9 (I)Michigan Michigan -4.6, 68-64 (67%)\nL, 77-58 \n22-10\n\n43\n-9.7",
-        "sagarin": "58",
-        "trank": "55"
+        "time": "Nov 20,  1:44 AM"
     },
     "warrennolan": {
-        "cwl": "(13-5)",
-        "group1": "1-6",
-        "group2": "2-3",
-        "group3": "9-1",
-        "group4": "10-0",
-        "rpi": "56",
-        "sos": "95",
-        "wl": "22-10"
+        "cwl": "(0-0)",
+        "group1": "0-0",
+        "group2": "0-0",
+        "group3": "2-0",
+        "group4": "2-0",
+        "rpi": "43",
+        "sos": "128",
+        "wl": "4-0"
     }
 }
