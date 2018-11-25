d92d1b185505adc46106df7ea9b1da22a482acd9
Ben Vankat <bvankat@gmail.com>
2018-11-25 15:47:02 -0600

    latest update

--- data.js
+++ data.js
@@ -23,21 +23,21 @@
         "composite": "15"
     },
     "sagarin": {
-        "sagarin_rate": "6"
+        "sagarin_rate": "11"
     },
     "teamrankings": {
-        "make_tournament": "77.1%",
-        "most_likely_seed": "#5 (9.0%)",
+        "make_tournament": "77.7%",
+        "most_likely_seed": "#5 (9.3%)",
         "projected_losses": "9.2",
         "projected_wins": "20.8",
         "rank": "12",
         "sos": "43"
     },
     "time": {
-        "time": "Nov 25,  4:57 AM"
+        "time": "Nov 25,  3:47 PM"
     },
     "trank": {
-        "trank": "17"
+        "trank": "16"
     },
     "warrennolan": {
         "cwl": "(0-0)",
@@ -45,8 +45,8 @@
         "group2": "0-0",
         "group3": "1-0",
         "group4": "4-0",
-        "rpi": "97",
-        "sos": "185",
+        "rpi": "101",
+        "sos": "198",
         "wl": "5-1"
     }
 }
