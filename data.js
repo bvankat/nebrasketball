8c0b87c5acc7effc0c6540266421d435cb47a154
Ben Vankat <bvankat@gmail.com>
2018-12-17 12:45:16 -0600

    latest update

--- data.js
+++ data.js
@@ -24,33 +24,33 @@
         "kpi_ranking": "26"
     },
     "massey": {
-        "composite": "15"
+        "composite": "13"
     },
     "ncaa": {
         "net_rank": "15"
     },
     "polls": {
-        "ap_rank": "NR",
+        "ap_rank": "25",
         "coaches_rank": "25"
     },
     "sagarin": {
-        "sagarin_rate": "12"
+        "sagarin_rate": "9"
     },
     "teamrankings": {
         "make_tournament": "85.1%",
-        "most_likely_seed": "#4 (11.2%)",
-        "projected_losses": "8.4",
-        "projected_wins": "21.6",
+        "most_likely_seed": "#3 (10.9%)",
+        "projected_losses": "8.3",
+        "projected_wins": "21.7",
         "rank": "12",
         "sos": "17"
     },
     "time": {
-        "time": "Dec 17,  4:43 AM"
+        "time": "Dec 17, 12:45 PM"
     },
     "trank": {
         "current_record": "9-2",
-        "trank": "9",
-        "trank_proj_record": "23-8 (13-7)"
+        "trank": "10",
+        "trank_proj_record": "22-9 (12-8)"
     },
     "warrennolan": {
         "current_conf_record": "1-1",
