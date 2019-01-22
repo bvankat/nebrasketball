097828fcd195299a7f8500bc7f5dbf205cf9739e
Ben Vankat <bvankat@gmail.com>
2019-01-22 08:25:56 -0600

    latest update

--- data.js
+++ data.js
@@ -18,17 +18,17 @@
         "sos": "21"
     },
     "espn_bracketology": {
-        "seed": "5-seed"
+        "seed": "8-seed"
     },
     "kenpom": {
-        "rating": "12",
-        "sos": "18"
+        "rating": "16",
+        "sos": "16"
     },
     "kpi_sports": {
         "kpi_ranking": "48"
     },
     "massey": {
-        "composite": "20"
+        "composite": "21"
     },
     "ncaa": {
         "net_rank": "13"
@@ -38,31 +38,31 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "10"
+        "sagarin_rate": "14"
     },
     "teamrankings": {
-        "make_tournament": "76.1%",
+        "make_tournament": "58.9%",
         "most_likely_seed": "7",
-        "projected_losses": "9.5",
-        "projected_wins": "21.5",
-        "rank": "10",
-        "sos": "19"
+        "projected_losses": "10.6",
+        "projected_wins": "20.4",
+        "rank": "13",
+        "sos": "17"
     },
     "time": {
-        "time": "Jan 22, 12:13 AM"
+        "time": "Jan 22,  8:25 AM"
     },
     "trank": {
-        "current_record": "13-5",
-        "trank": "7",
+        "current_record": "13-6",
+        "trank": "10",
         "trank_proj_record": "20-11 (10-10)",
         "trank_seed": "7"
     },
     "warrennolan": {
-        "current_conf_record": "3-4",
+        "current_conf_record": "3-5",
         "group1": "2-5",
         "group2": "4-0",
-        "group3": "2-0",
+        "group3": "2-1",
         "group4": "4-0",
-        "sos": "61"
+        "sos": "68"
     }
 }
