4cad64c0f1b12a2d869e3d3a23f5f0df1cc00526
Ben Vankat <bvankat@gmail.com>
2019-01-25 09:46:28 -0600

    latest update

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 {
     "bracketproject": {
-        "matrix_count": "86",
+        "matrix_count": "85",
         "matrix_seed": "7",
-        "total_brackets": "87"
+        "total_brackets": "86"
     },
     "cbs": {
-        "cbs_rank": "6"
+        "cbs_rank": "8"
     },
     "espn": {
         "bpi": "16",
-        "chance_sweet_16": "28.7%",
-        "proj_tourney_seed": "9",
-        "proj_win_loss_conf": "10.3-9.7",
-        "proj_win_loss_overall": "20.3-10.7",
-        "seven_day_change": "-4",
-        "sor": "36",
-        "sos": "19"
+        "chance_sweet_16": "29.5%",
+        "proj_tourney_seed": "10",
+        "proj_win_loss_conf": "10.4-9.6",
+        "proj_win_loss_overall": "20.4-10.6",
+        "seven_day_change": "-2",
+        "sor": "37",
+        "sos": "20"
     },
     "espn_bracketology": {
         "seed": "8-seed"
@@ -25,13 +25,13 @@
         "sos": "16"
     },
     "kpi_sports": {
-        "kpi_ranking": "48"
+        "kpi_ranking": "47"
     },
     "massey": {
         "composite": "21"
     },
     "ncaa": {
-        "net_rank": "20"
+        "net_rank": "21"
     },
     "polls": {
         "ap_rank": "NR",
@@ -41,15 +41,15 @@
         "sagarin_rate": "14"
     },
     "teamrankings": {
-        "make_tournament": "60.3%",
+        "make_tournament": "63.2%",
         "most_likely_seed": "7",
-        "projected_losses": "10.6",
-        "projected_wins": "20.4",
+        "projected_losses": "10.5",
+        "projected_wins": "20.5",
         "rank": "13",
         "sos": "18"
     },
     "time": {
-        "time": "Jan 23, 11:42 PM"
+        "time": "Jan 25,  9:46 AM"
     },
     "trank": {
         "current_record": "13-6",
@@ -60,8 +60,8 @@
     "warrennolan": {
         "current_conf_record": "3-5",
         "group1": "2-5",
-        "group2": "3-1",
-        "group3": "3-0",
+        "group2": "4-1",
+        "group3": "2-0",
         "group4": "4-0",
         "sos": "76"
     }
