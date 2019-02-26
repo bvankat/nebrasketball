7d6bcdf18b59e67ce597449d724cb1b8364e078b
Ben Vankat <bvankat@gmail.com>
2019-02-26 12:42:00 -0600

    latest update

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 {
     "bracketproject": {
-        "matrix_count": "7",
+        "matrix_count": "5",
         "matrix_seed": "NR",
-        "total_brackets": "109"
+        "total_brackets": "112"
     },
     "cbs": {
         "cbs_rank": "NR"
@@ -25,13 +25,13 @@
         "sos": "13"
     },
     "kpi_sports": {
-        "kpi_ranking": "68"
+        "kpi_ranking": "69"
     },
     "massey": {
-        "composite": "46"
+        "composite": "49"
     },
     "ncaa": {
-        "net_rank": "46"
+        "net_rank": "45"
     },
     "polls": {
         "ap_rank": "NR",
@@ -41,7 +41,7 @@
         "sagarin_rate": "39"
     },
     "teamrankings": {
-        "make_tournament": "1.3%",
+        "make_tournament": "1.4%",
         "most_likely_seed": "12",
         "projected_losses": "15.1",
         "projected_wins": "15.9",
@@ -49,7 +49,7 @@
         "sos": "34"
     },
     "time": {
-        "time": "Feb 25,  3:42 PM"
+        "time": "Feb 26, 12:41 PM"
     },
     "trank": {
         "current_record": "15-13",
