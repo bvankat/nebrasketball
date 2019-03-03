35f36a8cdfc2b436b6d90002abb201377d8d54c0
Ben Vankat <bvankat@gmail.com>
2019-03-03 15:45:50 -0600

    latest update

--- data.js
+++ data.js
@@ -49,7 +49,7 @@
         "sos": "39"
     },
     "time": {
-        "time": "Mar 03,  2:44 PM"
+        "time": "Mar 03,  3:45 PM"
     },
     "trank": {
         "current_record": "15-14",
@@ -64,6 +64,6 @@
         "group3": "3-0",
         "group4": "4-0",
         "rpi": "117",
-        "sos": "70"
+        "sos": "71"
     }
 }
