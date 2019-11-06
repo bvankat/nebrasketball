0d697ebfd8666ece0bdc1011d816c47585ecb27b
Ben Vankat <bvankat@gmail.com>
2019-11-06 09:39:03 -0600

    latest update

--- data.js
+++ data.js
@@ -1,69 +1,40 @@
 {
     "bracketproject": {
-        "matrix_count": "1",
+        "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "117"
-    },
-    "cbs": {
-        "cbs_rank": "NR"
+        "total_brackets": "46"
     },
     "espn": {
-        "bpi": "36",
-        "chance_sweet_16": "10.6%",
-        "proj_tourney_seed": "12",
-        "proj_win_loss_conf": "6.0-14.0",
-        "proj_win_loss_overall": "18.3-15.7",
-        "seven_day_change": "1",
-        "sor": "48",
-        "sos": "14"
+        "bpi": "90",
+        "chance_sweet_16": "0.0%",
+        "proj_tourney_seed": "NR",
+        "proj_win_loss_conf": "7.2-12.8",
+        "proj_win_loss_overall": "13.8-15.2",
+        "seven_day_change": "",
+        "sor": "1",
+        "sos": "1"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "40",
-        "sos": "4"
-    },
-    "kpi_sports": {
-        "kpi_ranking": "56"
-    },
-    "massey": {
-        "composite": "55"
-    },
-    "ncaa": {
-        "net_rank": "49"
+        "rating": "129",
+        "sos": "137"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "33"
-    },
-    "teamrankings": {
-        "make_tournament": "2.8%",
-        "most_likely_seed": "12",
-        "projected_losses": "15.0",
-        "projected_wins": "18.0",
-        "rank": "37",
-        "sos": "42"
+        "sagarin_rate": "73"
     },
     "time": {
-        "time": "Mar 15,  3:43 PM"
+        "time": "Nov 06,  9:39 AM"
     },
     "trank": {
-        "current_record": "18-15",
-        "trank": "41",
-        "trank_proj_record": "18-16 (6-14)",
+        "current_record": "0-1",
+        "trank": "113",
+        "trank_proj_record": "10-19 (5-15)",
         "trank_seed": "NR"
-    },
-    "warrennolan": {
-        "current_conf_record": "6-14",
-        "group1": "3-12",
-        "group2": "6-3",
-        "group3": "4-0",
-        "group4": "4-0",
-        "rpi": "83",
-        "sos": "43"
     }
 }
