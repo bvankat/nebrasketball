aa2ba220fd0b268d073d955676e32c9aa011a2ab
Ben Vankat <bvankat@gmail.com>
2019-11-07 14:43:16 -0600

    latest update

--- data.js
+++ data.js
@@ -4,44 +4,34 @@
         "matrix_seed": "NR",
         "total_brackets": "46"
     },
-    "espn": {
-        "bpi": "90",
-        "chance_sweet_16": "0.0%",
-        "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "7.2-12.8",
-        "proj_win_loss_overall": "13.8-15.2",
-        "seven_day_change": "",
-        "sor": "1",
-        "sos": "1"
-    },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "129",
-        "sos": "137"
+        "rating": "128",
+        "sos": "175"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "128"
+        "sagarin_rate": "119"
     },
     "teamrankings": {
-        "make_tournament": "2.8%",
+        "make_tournament": "2.9%",
         "most_likely_seed": "12",
         "projected_losses": "18.0",
         "projected_wins": "11.0",
-        "rank": "100",
-        "sos": "236"
+        "rank": "102",
+        "sos": "207"
     },
     "time": {
-        "time": "Nov 06,  3:41 PM"
+        "time": "Nov 07,  2:43 PM"
     },
     "trank": {
         "current_record": "0-1",
-        "trank": "113",
+        "trank": "117",
         "trank_proj_record": "10-19 (5-15)",
         "trank_seed": "NR"
     }
