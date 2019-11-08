55f8e8428aaa6ca7b9809353bc6fef03b96ea646
Ben Vankat <bvankat@gmail.com>
2019-11-08 12:44:07 -0600

    latest update

--- data.js
+++ data.js
@@ -9,25 +9,25 @@
     },
     "kenpom": {
         "rating": "127",
-        "sos": "180"
+        "sos": "182"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "119"
+        "sagarin_rate": "124"
     },
     "teamrankings": {
-        "make_tournament": "2.9%",
-        "most_likely_seed": "12",
-        "projected_losses": "18.0",
-        "projected_wins": "11.0",
+        "make_tournament": "3.1%",
+        "most_likely_seed": "11",
+        "projected_losses": "17.9",
+        "projected_wins": "11.1",
         "rank": "102",
-        "sos": "207"
+        "sos": "197"
     },
     "time": {
-        "time": "Nov 07, 11:25 PM"
+        "time": "Nov 08, 12:44 PM"
     },
     "trank": {
         "current_record": "0-1",
