d561d99a7a0ff51d48f53e30cd459ca8d459d213
Ben Vankat <bvankat@gmail.com>
2019-11-15 10:42:29 -0600

    latest update

--- data.js
+++ data.js
@@ -8,14 +8,14 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "106",
+        "bpi": "109",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "6.4-13.6",
-        "proj_win_loss_overall": "11.0-18.0",
-        "seven_day_change": "-3",
-        "sor": "309",
-        "sos": "317"
+        "proj_win_loss_conf": "6.3-13.7",
+        "proj_win_loss_overall": "10.8-18.2",
+        "seven_day_change": "-6",
+        "sor": "310",
+        "sos": "322"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -29,7 +29,7 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "97"
+        "sagarin_rate": "137"
     },
     "teamrankings": {
         "make_tournament": "0.7%",
@@ -40,7 +40,7 @@
         "sos": "256"
     },
     "time": {
-        "time": "Nov 15,  4:43 AM"
+        "time": "Nov 15, 10:42 AM"
     },
     "trank": {
         "current_record": "0-2",
@@ -54,7 +54,7 @@
         "group2": "0-0",
         "group3": "0-0",
         "group4": "0-2",
-        "rpi": "302",
-        "sos": "248"
+        "rpi": "307",
+        "sos": "254"
     }
 }
