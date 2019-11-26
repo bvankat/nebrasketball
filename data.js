102e9e1c68d1e185cf6ccba003c37eeb24c6fccd
Ben Vankat <bvankat@gmail.com>
2019-11-26 00:42:51 -0600

    latest update

--- data.js
+++ data.js
@@ -8,53 +8,53 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "114",
+        "bpi": "126",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "6.0-14.0",
-        "proj_win_loss_overall": "10.7-18.3",
-        "seven_day_change": "-5",
-        "sor": "283",
-        "sos": "341"
+        "proj_win_loss_conf": "5.6-14.4",
+        "proj_win_loss_overall": "10.3-18.7",
+        "seven_day_change": "-16",
+        "sor": "266",
+        "sos": "353"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "141",
-        "sos": "346"
+        "rating": "134",
+        "sos": "333"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "142"
+        "sagarin_rate": "144"
     },
     "teamrankings": {
-        "make_tournament": "0.6%",
-        "most_likely_seed": "9",
-        "projected_losses": "18.8",
-        "projected_wins": "10.2",
-        "rank": "108",
-        "sos": "291"
+        "make_tournament": "0.3%",
+        "most_likely_seed": "12",
+        "projected_losses": "19.4",
+        "projected_wins": "10.6",
+        "rank": "120",
+        "sos": "303"
     },
     "time": {
-        "time": "Nov 23,  1:41 AM"
+        "time": "Nov 26, 12:42 AM"
     },
     "trank": {
-        "current_record": "2-2",
-        "trank": "134",
-        "trank_proj_record": "9-20 (4-16)",
+        "current_record": "3-2",
+        "trank": "131",
+        "trank_proj_record": "10-20 (4-16)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
         "group1": "0-0",
-        "group2": "0-0",
-        "group3": "0-2",
-        "group4": "2-0",
-        "rpi": "338",
-        "sos": "341"
+        "group2": "0-1",
+        "group3": "0-1",
+        "group4": "3-0",
+        "rpi": "291",
+        "sos": "311"
     }
 }
