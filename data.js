4ac2d033a9ca3b95c5d3465ce2169c0a86399236
Ben Vankat <bvankat@gmail.com>
2019-12-08 02:58:15 -0600

    latest update

--- data.js
+++ data.js
@@ -8,53 +8,53 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "132",
+        "bpi": "140",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "4.8-15.2",
-        "proj_win_loss_overall": "10.9-20.1",
-        "seven_day_change": "3",
-        "sor": "184",
-        "sos": "324"
+        "proj_win_loss_conf": "4.5-15.5",
+        "proj_win_loss_overall": "10.4-20.6",
+        "seven_day_change": "-10",
+        "sor": "202",
+        "sos": "255"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "146",
-        "sos": "254"
+        "rating": "154",
+        "sos": "195"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "139"
+        "sagarin_rate": "150"
     },
     "teamrankings": {
-        "make_tournament": "0.1%",
-        "most_likely_seed": "9",
-        "projected_losses": "20.5",
-        "projected_wins": "10.5",
-        "rank": "127",
-        "sos": "147"
+        "make_tournament": "0.0%",
+        "most_likely_seed": "10",
+        "projected_losses": "21.2",
+        "projected_wins": "9.8",
+        "rank": "128",
+        "sos": "141"
     },
     "time": {
-        "time": "Dec 05,  3:41 AM"
+        "time": "Dec 08,  2:58 AM"
     },
     "trank": {
-        "current_record": "4-4",
-        "trank": "153",
-        "trank_proj_record": "9-22 (3-17)",
+        "current_record": "4-5",
+        "trank": "174",
+        "trank_proj_record": "8-23 (3-17)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
         "group1": "0-2",
-        "group2": "0-0",
+        "group2": "0-1",
         "group3": "1-1",
         "group4": "3-1",
-        "rpi": "184",
-        "sos": "164"
+        "rpi": "166",
+        "sos": "119"
     }
 }
