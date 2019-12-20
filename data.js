97658bc8c648b1b7de38b4aa9900df53a832f551
Ben Vankat <bvankat@gmail.com>
2019-12-20 11:41:37 -0600

    latest update

--- data.js
+++ data.js
@@ -8,46 +8,46 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "135",
+        "bpi": "137",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "5.3-14.7",
-        "proj_win_loss_overall": "11.0-20.0",
+        "proj_win_loss_conf": "5.2-14.8",
+        "proj_win_loss_overall": "10.9-20.1",
         "seven_day_change": "13",
-        "sor": "175",
-        "sos": "66"
+        "sor": "171",
+        "sos": "65"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "129",
-        "sos": "86"
+        "rating": "128",
+        "sos": "84"
     },
     "ncaa": {
-        "net_rank": "151"
+        "net_rank": "162"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "113"
+        "sagarin_rate": "118"
     },
     "teamrankings": {
         "make_tournament": "0.1%",
         "most_likely_seed": "12",
-        "projected_losses": "20.1",
-        "projected_wins": "10.9",
-        "rank": "115",
-        "sos": "65"
+        "projected_losses": "20.2",
+        "projected_wins": "10.8",
+        "rank": "116",
+        "sos": "63"
     },
     "time": {
-        "time": "Dec 18, 10:41 PM"
+        "time": "Dec 20, 11:41 AM"
     },
     "trank": {
         "current_record": "5-6",
-        "trank": "139",
+        "trank": "138",
         "trank_proj_record": "10-21 (4-16)",
         "trank_seed": "NR"
     },
@@ -55,9 +55,9 @@
         "current_conf_record": "1-1",
         "group1": "0-3",
         "group2": "1-0",
-        "group3": "1-2",
-        "group4": "3-1",
-        "rpi": "127",
-        "sos": "56"
+        "group3": "0-2",
+        "group4": "4-1",
+        "rpi": "131",
+        "sos": "54"
     }
 }
