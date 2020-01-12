c5f8fe07e2a63c02e2d418447187c0ce73644443
Ben Vankat <bvankat@gmail.com>
2020-01-12 11:43:02 -0600

    latest update

--- data.js
+++ data.js
@@ -25,30 +25,30 @@
         "sos": "65"
     },
     "ncaa": {
-        "net_rank": "152"
+        "net_rank": "159"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "110"
+        "sagarin_rate": "116"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "13",
-        "projected_losses": "20.8",
-        "projected_wins": "10.2",
+        "most_likely_seed": "15",
+        "projected_losses": "21.2",
+        "projected_wins": "9.8",
         "rank": "115",
         "sos": "76"
     },
     "time": {
-        "time": "Jan 12,  3:42 AM"
+        "time": "Jan 12, 11:43 AM"
     },
     "trank": {
         "current_record": "7-9",
-        "trank": "133",
-        "trank_proj_record": "10-21 (5-15)",
+        "trank": "135",
+        "trank_proj_record": "9-22 (4-16)",
         "trank_seed": "NR"
     },
     "warrennolan": {
