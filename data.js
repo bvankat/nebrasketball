f49a9fa9beede765f67bbf03e893fd1aa756a62d
Ben Vankat <bvankat@gmail.com>
2020-03-02 23:31:36 -0600

    latest update

--- data.js
+++ data.js
@@ -2,62 +2,62 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "87"
+        "total_brackets": "108"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "138",
+        "bpi": "159",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "4.5-15.5",
-        "proj_win_loss_overall": "9.5-21.5",
-        "seven_day_change": "9",
-        "sor": "183",
-        "sos": "44"
+        "proj_win_loss_conf": "2.1-17.9",
+        "proj_win_loss_overall": "7.1-23.9",
+        "seven_day_change": "-11",
+        "sor": "198",
+        "sos": "14"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "129",
-        "sos": "40"
+        "rating": "145",
+        "sos": "18"
     },
     "ncaa": {
-        "net_rank": "157"
+        "net_rank": "195"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "115"
+        "sagarin_rate": "160"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "14",
-        "projected_losses": "21.1",
-        "projected_wins": "9.9",
-        "rank": "115",
-        "sos": "68"
+        "most_likely_seed": "0",
+        "projected_losses": "23.8",
+        "projected_wins": "7.2",
+        "rank": "127",
+        "sos": "57"
     },
     "time": {
-        "time": "Jan 15,  1:44 PM"
+        "time": "Mar 02, 11:31 PM"
     },
     "trank": {
-        "current_record": "7-10",
-        "trank": "129",
-        "trank_proj_record": "10-21 (5-15)",
+        "current_record": "7-22",
+        "trank": "147",
+        "trank_proj_record": "7-24 (2-18)",
         "trank_seed": "NR"
     },
     "warrennolan": {
-        "current_conf_record": "2-4",
-        "group1": "0-5",
-        "group2": "2-1",
-        "group3": "2-2",
-        "group4": "3-2",
-        "rpi": "169",
-        "sos": "70"
+        "current_conf_record": "2-16",
+        "group1": "0-8",
+        "group2": "1-8",
+        "group3": "4-2",
+        "group4": "2-4",
+        "rpi": "248",
+        "sos": "69"
     }
 }
