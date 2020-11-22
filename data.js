f91cc528739a3baea061bd7465428bc8d8f25c85
Ben Vankat <bvankat@gmail.com>
2020-11-21 22:38:35 -0600

    latest update

--- data.js
+++ data.js
@@ -2,62 +2,41 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "108"
-    },
-    "cbs": {
-        "cbs_rank": "NR"
+        "total_brackets": "97"
     },
     "espn": {
-        "bpi": "159",
+        "bpi": "174",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "2.1-17.9",
-        "proj_win_loss_overall": "7.1-23.9",
-        "seven_day_change": "-11",
-        "sor": "198",
-        "sos": "14"
+        "proj_win_loss_conf": "2.0-18.0",
+        "proj_win_loss_overall": "7.0-25.0",
+        "seven_day_change": "-16",
+        "sor": "200",
+        "sos": "8"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "kenpom": {
-        "rating": "145",
-        "sos": "18"
-    },
-    "ncaa": {
-        "net_rank": "195"
-    },
-    "polls": {
-        "ap_rank": "NR",
-        "coaches_rank": "NR"
-    },
-    "sagarin": {
-        "sagarin_rate": "160"
-    },
-    "teamrankings": {
-        "make_tournament": "0.0%",
-        "most_likely_seed": "0",
-        "projected_losses": "23.8",
-        "projected_wins": "7.2",
-        "rank": "127",
-        "sos": "57"
+        "rating": "117",
+        "sos": "1"
     },
     "time": {
-        "time": "Mar 02, 11:31 PM"
+        "time": "Nov 21, 10:38 PM"
     },
     "trank": {
-        "current_record": "7-22",
-        "trank": "147",
-        "trank_proj_record": "7-24 (2-18)",
+        "current_record": "0-0\u2013",
+        "trank": "102",
+        "trank_proj_record": "9-16 (5-15)",
         "trank_seed": "NR"
     },
     "warrennolan": {
-        "current_conf_record": "2-16",
-        "group1": "0-8",
-        "group2": "1-8",
-        "group3": "4-2",
-        "group4": "2-4",
-        "rpi": "248",
-        "sos": "69"
+        "current_conf_record": "0-0",
+        "group1": "0-0",
+        "group2": "0-0",
+        "group3": "0-0",
+        "group4": "0-0",
+        "rpi": "251",
+        "sos": "0"
     }
 }
