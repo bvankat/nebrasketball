9fe91c791a1bb2c03818b6df5a3cad78378a0968
Ben Vankat <bvankat@gmail.com>
2020-11-26 12:46:58 -0600

    latest update

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "97"
+        "total_brackets": "31"
     },
     "espn": {
-        "bpi": "98",
-        "chance_round_32": "0.1%",
-        "chance_sweet_16": "0.0%",
+        "bpi": "90",
+        "chance_round_32": "0.2%",
+        "chance_sweet_16": "0.1%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "6.1-11.9",
         "proj_win_loss_overall": "11.3-13.7",
         "seven_day_change": "",
-        "sor": "1",
-        "sos": "1"
+        "sor": "63",
+        "sos": "143"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -21,36 +21,36 @@
     "kenpom": {
         "conference_record": "4-14",
         "overall_record": "8-17",
-        "rating": "108",
-        "sos": "22"
+        "rating": "109",
+        "sos": "151"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "teamrankings": {
-        "make_tournament": "0.9%",
-        "most_likely_seed": "10",
-        "projected_losses": "17.2",
-        "projected_wins": "5.8",
-        "rank": "139"
+        "make_tournament": "4.0%",
+        "most_likely_seed": "7",
+        "projected_losses": "15.1",
+        "projected_wins": "7.9",
+        "rank": "99"
     },
     "time": {
-        "time": "Nov 25,  3:43 PM"
+        "time": "Nov 26, 12:46 PM"
     },
     "trank": {
         "current_record": "1-00\u20130",
-        "trank": "89",
+        "trank": "90",
         "trank_proj_record": "11-16 (6-14)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "group1": "1-0",
+        "group1": "0-0",
         "group2": "0-0",
-        "group3": "0-0",
+        "group3": "1-0",
         "group4": "0-0",
-        "rpi": "1",
-        "sos": "1"
+        "rpi": "2",
+        "sos": "3"
     }
 }
