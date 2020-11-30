f53f19174aed1e30b761201805f9d69a82fa59b6
Ben Vankat <bvankat@gmail.com>
2020-11-30 00:29:14 -0600

    latest update

--- data.js
+++ data.js
@@ -5,15 +5,15 @@
         "total_brackets": "31"
     },
     "espn": {
-        "bpi": "95",
+        "bpi": "99",
         "chance_round_32": "0.1%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "5.9-12.1",
+        "proj_win_loss_conf": "5.8-12.2",
         "proj_win_loss_overall": "10.4-14.6",
         "seven_day_change": "",
-        "sor": "138",
-        "sos": "141"
+        "sor": "131",
+        "sos": "180"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -21,42 +21,42 @@
     "kenpom": {
         "conference_record": "4-14",
         "overall_record": "8-17",
-        "rating": "105",
+        "rating": "104",
         "record": "2-1",
-        "sos": "167"
+        "sos": "171"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "90"
+        "sagarin_rate": "80"
     },
     "teamrankings": {
-        "make_tournament": "1.7%",
-        "most_likely_seed": "8",
-        "proj_conf_record": "4.0-14.0",
-        "projected_losses": "16.9",
-        "projected_wins": "8.1",
-        "rank": "108"
+        "make_tournament": "4.8%",
+        "most_likely_seed": "10",
+        "proj_conf_record": "4.5-13.5",
+        "projected_losses": "16.0",
+        "projected_wins": "9.0",
+        "rank": "96"
     },
     "time": {
-        "time": "Nov 28, 10:27 PM"
+        "time": "Nov 30, 12:29 AM"
     },
     "trank": {
-        "trank": "84",
+        "trank": "85",
         "trank_proj_record": "10-17 (6-14)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "elo": "233",
-        "group1": "0-1",
-        "group2": "0-0",
+        "elo": "232",
+        "group1": "0-0",
+        "group2": "0-1",
         "group3": "2-0",
         "group4": "0-0",
         "record": "2-1",
-        "rpi": "112",
-        "sos": "101"
+        "rpi": "129",
+        "sos": "121"
     }
 }
