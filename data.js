542032cb9d4465bf819ab85b91063f8a52fa5647
Ben Vankat <bvankat@gmail.com>
2020-12-06 22:29:27 -0600

    latest update

--- data.js
+++ data.js
@@ -5,15 +5,15 @@
         "total_brackets": "31"
     },
     "espn": {
-        "bpi": "98",
-        "chance_round_32": "0.1%",
+        "bpi": "99",
+        "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "5.5-12.5",
-        "proj_win_loss_overall": "9.3-14.7",
-        "seven_day_change": "-3",
-        "sor": "149",
-        "sos": "242"
+        "proj_win_loss_conf": "6.2-13.8",
+        "proj_win_loss_overall": "9.9-16.1",
+        "seven_day_change": "0",
+        "sor": "148",
+        "sos": "249"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -21,42 +21,42 @@
     "kenpom": {
         "conference_record": "4-16",
         "overall_record": "8-18",
-        "rating": "108",
+        "rating": "107",
         "record": "3-1",
-        "sos": "231"
+        "sos": "232"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "77"
+        "sagarin_rate": "74"
     },
     "teamrankings": {
-        "make_tournament": "4.0%",
+        "make_tournament": "4.2%",
         "most_likely_seed": "11",
-        "proj_conf_record": "4.7-15.3",
-        "projected_losses": "17.7",
-        "projected_wins": "8.3",
-        "rank": "99"
+        "proj_conf_record": "4.8-15.2",
+        "projected_losses": "17.5",
+        "projected_wins": "8.5",
+        "rank": "98"
     },
     "time": {
-        "time": "Dec 05, 10:29 PM"
+        "time": "Dec 06, 10:29 PM"
     },
     "trank": {
-        "trank": "90",
+        "trank": "92",
         "trank_proj_record": "9-17 (6-14)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "elo": "210",
+        "elo": "208",
         "group1": "0-0",
         "group2": "0-1",
-        "group3": "2-0",
-        "group4": "1-0",
+        "group3": "1-0",
+        "group4": "2-0",
         "record": "3-1",
-        "rpi": "206",
-        "sos": "225"
+        "rpi": "212",
+        "sos": "235"
     }
 }
