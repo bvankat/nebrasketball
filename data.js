6fa7179958289b4569dd3212b88dfd0bba2e8068
Ben Vankat <bvankat@gmail.com>
2020-12-10 13:29:35 -0600

    latest update

--- data.js
+++ data.js
@@ -5,15 +5,15 @@
         "total_brackets": "31"
     },
     "espn": {
-        "bpi": "95",
+        "bpi": "103",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "6.1-13.9",
-        "proj_win_loss_overall": "9.8-16.2",
-        "seven_day_change": "5",
-        "sor": "151",
-        "sos": "270"
+        "proj_win_loss_conf": "5.9-14.1",
+        "proj_win_loss_overall": "9.0-17.0",
+        "seven_day_change": "-5",
+        "sor": "189",
+        "sos": "246"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -30,22 +30,22 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "76"
+        "sagarin_rate": "83"
     },
     "teamrankings": {
-        "make_tournament": "1.6%",
+        "make_tournament": "1.7%",
         "most_likely_seed": "11",
-        "proj_conf_record": "4.2-15.8",
+        "proj_conf_record": "4.1-15.9",
         "projected_losses": "18.7",
         "projected_wins": "7.3",
         "rank": "108"
     },
     "time": {
-        "time": "Dec 10,  4:29 AM"
+        "time": "Dec 10,  1:29 PM"
     },
     "trank": {
-        "trank": "94",
-        "trank_proj_record": "9-17 (5-15)",
+        "trank": "100",
+        "trank_proj_record": "8-18 (5-15)",
         "trank_seed": "NR"
     },
     "warrennolan": {
@@ -56,7 +56,7 @@
         "group3": "0-0",
         "group4": "3-0",
         "record": "3-2",
-        "rpi": "269",
-        "sos": "271"
+        "rpi": "268",
+        "sos": "270"
     }
 }
