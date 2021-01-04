e93c855a8bf6e275946c2c33eedf1cd577b4217c
Ben Vankat <bvankat@gmail.com>
2021-01-04 16:30:01 -0600

    latest update

--- data.js
+++ data.js
@@ -5,15 +5,15 @@
         "total_brackets": "36"
     },
     "espn": {
-        "bpi": "125",
+        "bpi": "124",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "3.7-16.3",
         "proj_win_loss_overall": "7.7-19.3",
-        "seven_day_change": "-14",
-        "sor": "229",
-        "sos": "46"
+        "seven_day_change": "-13",
+        "sor": "223",
+        "sos": "42"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -34,14 +34,14 @@
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "13",
+        "most_likely_seed": "12",
         "proj_conf_record": "2.5-17.5",
         "projected_losses": "20.5",
         "projected_wins": "6.5",
         "rank": "125"
     },
     "time": {
-        "time": "Jan 04,  3:30 AM"
+        "time": "Jan 04,  4:30 PM"
     },
     "trank": {
         "trank": "103",
@@ -51,10 +51,10 @@
     "warrennolan": {
         "current_conf_record": "0-4",
         "elo": "236",
-        "quadrant_1": "0-5",
-        "quadrant_2": "0-1",
-        "quadrant_3": "1-1",
-        "quadrant_4": "2-0",
+        "quadrant_1": "0-4",
+        "quadrant_2": "0-0",
+        "quadrant_3": "0-3",
+        "quadrant_4": "3-0",
         "record": "3-7",
         "rpi": "194",
         "sos": "67"
