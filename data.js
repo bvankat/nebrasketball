82613ebe88ad8f00b84f84836e50ac742e0086ce
Ben Vankat <bvankat@gmail.com>
2021-01-07 11:29:30 -0600

    latest update

--- data.js
+++ data.js
@@ -5,15 +5,15 @@
         "total_brackets": "36"
     },
     "espn": {
-        "bpi": "123",
+        "bpi": "119",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "3.5-15.5",
-        "proj_win_loss_overall": "7.5-18.5",
-        "seven_day_change": "-10",
-        "sor": "220",
-        "sos": "45"
+        "proj_win_loss_conf": "3.6-15.4",
+        "proj_win_loss_overall": "7.6-18.4",
+        "seven_day_change": "5",
+        "sor": "221",
+        "sos": "43"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -30,7 +30,7 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "114"
+        "sagarin_rate": "115"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
@@ -41,7 +41,7 @@
         "rank": "122"
     },
     "time": {
-        "time": "Jan 07,  6:29 AM"
+        "time": "Jan 07, 11:29 AM"
     },
     "trank": {
         "trank": "99",
