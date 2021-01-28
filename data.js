76db533f8a579117c99c511c26b91c9ec2849ede
Ben Vankat <bvankat@gmail.com>
2021-01-28 11:30:03 -0600

    latest update

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "74"
+        "total_brackets": "77"
     },
     "espn": {
         "bpi": "123",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "2.0-12.0",
+        "proj_win_loss_conf": "1.9-12.1",
         "proj_win_loss_overall": "6.0-15.0",
         "seven_day_change": "2",
-        "sor": "191",
-        "sos": "39"
+        "sor": "190",
+        "sos": "42"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -21,45 +21,45 @@
     "kenpom": {
         "conference_record": "2-12",
         "overall_record": "6-15",
-        "rating": "126",
+        "rating": "125",
         "record": "4-8",
-        "sos": "28"
+        "sos": "27"
     },
     "ncaa": {
-        "net_rank": "165"
+        "net_rank": "164"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "108"
+        "sagarin_rate": "107"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "12",
+        "most_likely_seed": "11",
         "proj_conf_record": "2.4-16.6",
         "projected_losses": "19.6",
         "projected_wins": "6.4",
-        "rank": "123"
+        "rank": "122"
     },
     "time": {
-        "time": "Jan 27, 12:33 PM"
+        "time": "Jan 28, 11:30 AM"
     },
     "trank": {
-        "trank": "96",
+        "trank": "97",
         "trank_proj_record": "8-19 (4-16)",
         "trank_seed": "NR"
     },
     "warrennolan": {
         "current_conf_record": "0-5",
-        "elo": "234",
+        "elo": "236",
         "quadrant_1": "0-4",
         "quadrant_2": "0-2",
         "quadrant_3": "0-2",
         "quadrant_4": "3-0",
         "record": "3-8",
-        "rpi": "216",
-        "sos": "61"
+        "rpi": "207",
+        "sos": "51"
     }
 }
