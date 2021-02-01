2048ba8fbb4133cee5369f93ec852346675c159c
Ben Vankat <bvankat@gmail.com>
2021-02-01 10:29:59 -0600

    latest update

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "78"
+        "total_brackets": "80"
     },
     "espn": {
-        "bpi": "121",
+        "bpi": "124",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "2.0-12.0",
         "proj_win_loss_overall": "6.0-15.0",
-        "seven_day_change": "4",
-        "sor": "184",
-        "sos": "41"
+        "seven_day_change": "-1",
+        "sor": "176",
+        "sos": "43"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -21,12 +21,12 @@
     "kenpom": {
         "conference_record": "2-12",
         "overall_record": "6-15",
-        "rating": "124",
+        "rating": "123",
         "record": "4-8",
-        "sos": "29"
+        "sos": "26"
     },
     "ncaa": {
-        "net_rank": "160"
+        "net_rank": "164"
     },
     "polls": {
         "ap_rank": "NR",
@@ -37,17 +37,17 @@
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "12",
+        "most_likely_seed": "13",
         "proj_conf_record": "2.4-16.6",
         "projected_losses": "19.6",
         "projected_wins": "6.4",
-        "rank": "122"
+        "rank": "121"
     },
     "time": {
-        "time": "Jan 31,  3:30 AM"
+        "time": "Feb 01, 10:29 AM"
     },
     "trank": {
-        "trank": "99",
+        "trank": "98",
         "trank_proj_record": "8-19 (4-16)",
         "trank_seed": "NR"
     },
@@ -56,10 +56,10 @@
         "elo": "237",
         "quadrant_1": "0-4",
         "quadrant_2": "0-2",
-        "quadrant_3": "1-2",
-        "quadrant_4": "2-0",
+        "quadrant_3": "0-2",
+        "quadrant_4": "3-0",
         "record": "3-8",
-        "rpi": "217",
-        "sos": "54"
+        "rpi": "215",
+        "sos": "52"
     }
 }
