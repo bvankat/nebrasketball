49bc356ce7bfced78da9dd9689f01cfb0bd79cc1
Ben Vankat <bvankat@gmail.com>
2021-02-05 03:29:46 -0600

    latest update

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "93"
+        "total_brackets": "92"
     },
     "espn": {
         "bpi": "125",
@@ -21,7 +21,7 @@
     "kenpom": {
         "conference_record": "2-12",
         "overall_record": "6-15",
-        "rating": "122",
+        "rating": "123",
         "record": "4-8",
         "sos": "28"
     },
@@ -41,10 +41,10 @@
         "proj_conf_record": "2.4-16.6",
         "projected_losses": "19.6",
         "projected_wins": "6.4",
-        "rank": "121"
+        "rank": "122"
     },
     "time": {
-        "time": "Feb 04, 10:29 PM"
+        "time": "Feb 05,  3:29 AM"
     },
     "trank": {
         "trank": "98",
@@ -59,7 +59,7 @@
         "quadrant_3": "0-2",
         "quadrant_4": "3-0",
         "record": "3-8",
-        "rpi": "224",
+        "rpi": "225",
         "sos": "57"
     }
 }
