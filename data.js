27ccb8fedf5473cbec2795ee2d7ee7f7ab9e2bfa
Ben Vankat <bvankat@gmail.com>
2021-02-05 11:30:08 -0600

    latest update

--- data.js
+++ data.js
@@ -5,15 +5,15 @@
         "total_brackets": "92"
     },
     "espn": {
-        "bpi": "125",
+        "bpi": "124",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "1.9-12.1",
         "proj_win_loss_overall": "5.9-15.1",
         "seven_day_change": "-2",
-        "sor": "177",
-        "sos": "48"
+        "sor": "173",
+        "sos": "45"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -26,28 +26,28 @@
         "sos": "28"
     },
     "ncaa": {
-        "net_rank": "170"
+        "net_rank": "167"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "118"
+        "sagarin_rate": "121"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "11",
+        "most_likely_seed": "12",
         "proj_conf_record": "2.4-16.6",
         "projected_losses": "19.6",
         "projected_wins": "6.4",
         "rank": "122"
     },
     "time": {
-        "time": "Feb 05,  4:30 AM"
+        "time": "Feb 05, 11:30 AM"
     },
     "trank": {
-        "trank": "98",
+        "trank": "99",
         "trank_proj_record": "8-19 (4-16)",
         "trank_seed": "NR"
     },
