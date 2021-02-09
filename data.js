34e033e29a81c5132316e3d6b67a91b3edfbe88f
Ben Vankat <bvankat@gmail.com>
2021-02-09 10:36:36 -0600

    latest update

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "94"
+        "total_brackets": "95"
     },
     "espn": {
-        "bpi": "125",
+        "bpi": "128",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "1.8-12.2",
-        "proj_win_loss_overall": "5.7-15.3",
+        "proj_win_loss_conf": "1.6-13.4",
+        "proj_win_loss_overall": "5.6-16.4",
         "seven_day_change": "-1",
-        "sor": "178",
-        "sos": "38"
+        "sor": "182",
+        "sos": "35"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -33,33 +33,33 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "123"
+        "sagarin_rate": "126"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "0",
-        "proj_conf_record": "2.1-16.9",
-        "projected_losses": "19.9",
-        "projected_wins": "6.1",
-        "rank": "123"
+        "most_likely_seed": "13",
+        "proj_conf_record": "1.9-17.1",
+        "projected_losses": "20.1",
+        "projected_wins": "5.9",
+        "rank": "128"
     },
     "time": {
-        "time": "Feb 08, 12:29 PM"
+        "time": "Feb 09, 10:36 AM"
     },
     "trank": {
-        "trank": "109",
+        "trank": "114",
         "trank_proj_record": "7-20 (3-17)",
         "trank_seed": "NR"
     },
     "warrennolan": {
-        "current_conf_record": "0-6",
-        "elo": "246",
-        "quadrant_1": "0-4",
+        "current_conf_record": "0-7",
+        "elo": "250",
+        "quadrant_1": "0-5",
         "quadrant_2": "0-3",
         "quadrant_3": "1-2",
         "quadrant_4": "2-0",
-        "record": "3-9",
+        "record": "3-10",
         "rpi": "223",
-        "sos": "49"
+        "sos": "40"
     }
 }
