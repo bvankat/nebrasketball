dc0836ed207a5d1802cbd65eb724f4ebae5524be
Ben Vankat <bvankat@gmail.com>
2021-02-24 12:29:43 -0600

    latest update

--- data.js
+++ data.js
@@ -2,18 +2,18 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "111"
+        "total_brackets": "120"
     },
     "espn": {
         "bpi": "127",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "2.3-16.7",
-        "proj_win_loss_overall": "6.3-19.7",
-        "seven_day_change": "-6",
-        "sor": "170",
-        "sos": "12"
+        "proj_win_loss_conf": "2.0-17.0",
+        "proj_win_loss_overall": "6.0-20.0",
+        "seven_day_change": "-2",
+        "sor": "177",
+        "sos": "14"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -21,48 +21,48 @@
     "kenpom": {
         "conference_record": "2-17",
         "overall_record": "6-20",
-        "rating": "117",
-        "record": "5-15",
+        "rating": "115",
+        "record": "5-16",
         "sos": "4"
     },
     "kpi_sports": {
         "kpi_ranking": "190"
     },
     "ncaa": {
-        "net_rank": "147"
+        "net_rank": "145"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "113"
+        "sagarin_rate": "112"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "NR",
-        "proj_conf_record": "2.1-16.9",
-        "projected_losses": "19.9",
-        "projected_wins": "6.1",
+        "most_likely_seed": "15",
+        "proj_conf_record": "1.9-17.1",
+        "projected_losses": "20.1",
+        "projected_wins": "5.9",
         "rank": "123"
     },
     "time": {
-        "time": "Feb 23, 12:30 PM"
+        "time": "Feb 24, 12:29 PM"
     },
     "trank": {
-        "trank": "108",
+        "trank": "107",
         "trank_proj_record": "6-20 (2-17)",
         "trank_seed": "NR"
     },
     "warrennolan": {
-        "current_conf_record": "1-12",
-        "elo": "242",
-        "quadrant_1": "1-10",
-        "quadrant_2": "0-3",
-        "quadrant_3": "0-2",
+        "current_conf_record": "1-13",
+        "elo": "255",
+        "quadrant_1": "1-11",
+        "quadrant_2": "0-4",
+        "quadrant_3": "0-1",
         "quadrant_4": "3-0",
-        "record": "4-15",
-        "rpi": "217",
-        "sos": "32"
+        "record": "4-16",
+        "rpi": "233",
+        "sos": "34"
     }
 }
