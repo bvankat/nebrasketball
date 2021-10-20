278430e9b132208f2b701556c501d58018cbb754
Ben Vankat <bvankat@gmail.com>
2021-10-20 11:34:52 -0500

    latest update

--- data.js
+++ data.js
@@ -1,68 +1,22 @@
 {
-    "bracketproject": {
-        "matrix_count": 0,
-        "matrix_seed": "NR",
-        "total_brackets": "122"
-    },
-    "espn": {
-        "bpi": "121",
-        "chance_round_32": "0.0%",
-        "chance_sweet_16": "0.0%",
-        "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "3.3-15.7",
-        "proj_win_loss_overall": "7.3-18.7",
-        "seven_day_change": "4",
-        "sor": "148",
-        "sos": "9"
-    },
-    "espn_bracketology": {
-        "seed": "NR"
-    },
     "kenpom": {
-        "conference_record": "3-16",
-        "overall_record": "7-20",
-        "rating": "95",
-        "record": "7-20",
-        "sos": "4"
-    },
-    "kpi_sports": {
-        "kpi_ranking": "182"
-    },
-    "ncaa": {
-        "net_rank": "126"
+        "conference_record": "6-14",
+        "overall_record": "14-17",
+        "rating": "81",
+        "record": "0-0",
+        "sos": "1"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
-    "sagarin": {
-        "sagarin_rate": "103"
-    },
-    "teamrankings": {
-        "make_tournament": "0.0%",
-        "most_likely_seed": "NR",
-        "proj_conf_record": "3-16",
-        "projected_losses": "19",
-        "projected_wins": "7",
-        "rank": "113"
-    },
     "time": {
-        "time": "Mar 15,  5:41 AM"
+        "time": "Oct 20, 11:34 AM"
     },
     "trank": {
-        "trank": "86",
-        "trank_proj_record": "7-19 (3-16)",
+        "trank": "66",
+        "trank_make_tourney": "9.5",
+        "trank_proj_record": "16-15 (8-12)",
         "trank_seed": "NR"
-    },
-    "warrennolan": {
-        "current_conf_record": "3-16",
-        "elo": "190",
-        "quadrant_1": "1-12",
-        "quadrant_2": "1-5",
-        "quadrant_3": "1-1",
-        "quadrant_4": "3-0",
-        "record": "7-20",
-        "rpi": "243",
-        "sos": "43"
     }
 }
