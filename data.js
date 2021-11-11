aa1d2b6944db45ca7c628d659adcfcc3041d5117
Ben Vankat <bvankat@gmail.com>
2021-11-11 09:42:52 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -15,8 +15,8 @@
         "proj_win_loss_conf": "7.3-12.7",
         "proj_win_loss_overall": "14.3-16.7",
         "seven_day_change": "-9",
-        "sor": "185",
-        "sos": "161"
+        "sor": "211",
+        "sos": "182"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -24,9 +24,9 @@
     "kenpom": {
         "conference_record": "6-14",
         "overall_record": "13-18",
-        "rating": "90",
+        "rating": "91",
         "record": "0-1",
-        "sos": "156"
+        "sos": "173"
     },
     "kpi_sports": {
         "kpi_ranking": 201
@@ -36,26 +36,26 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "89"
+        "sagarin_rate": "88"
     },
     "teamrankings": {
-        "make_tournament": "7.7%",
+        "make_tournament": "8.3%",
         "most_likely_seed": "12",
-        "proj_conf_record": "5.4-14.6",
-        "projected_losses": "18.8",
-        "projected_wins": "12.2",
-        "rank": "97"
+        "proj_conf_record": "5.6-14.4",
+        "projected_losses": "18.6",
+        "projected_wins": "12.4",
+        "rank": "95"
     },
     "time": {
-        "time": "Nov 10,  4:04 PM"
+        "time": "Nov 11,  9:42 AM"
     },
     "trank": {
         "trank": "76",
-        "trank_make_tourney": "0.8",
+        "trank_make_tourney": "1.0",
         "trank_proj_record": "15-16 (7-13)",
         "trank_seed": "NR",
-        "wab_rank": "348",
-        "wab_score": "-0.9348"
+        "wab_rank": "344",
+        "wab_score": "-0.9344"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
@@ -66,7 +66,7 @@
         "quadrant_3": "0-0",
         "quadrant_4": "0-1",
         "record": "0-1",
-        "rpi": "67",
+        "rpi": "141",
         "sos": "1"
     }
 }
