ee30bf291d33f3040d4c2d86d154cde67be49f72
Ben Vankat <bvankat@gmail.com>
2021-12-07 10:31:27 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -12,40 +12,40 @@
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "6.2-13.8",
+        "proj_win_loss_conf": "6.1-13.9",
         "proj_win_loss_overall": "12.8-18.2",
-        "seven_day_change": "0",
-        "sor": "205",
-        "sos": "216"
+        "seven_day_change": "4",
+        "sor": "202",
+        "sos": "215"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "105"
+        "miya_rating": "107"
     },
     "haslametrics": {
-        "haslam_rating": "120"
+        "haslam_rating": "119"
     },
     "kenpom": {
         "conference_record": "5-15",
         "overall_record": "12-19",
         "rating": "102",
         "record": "5-4",
-        "sos": "292"
+        "sos": "293"
     },
     "kpi_sports": {
         "kpi_ranking": 201
     },
     "ncaa": {
-        "net_rank": "192"
+        "net_rank": "191"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "109"
+        "sagarin_rate": "108"
     },
     "teamrankings": {
         "make_tournament": "1.6%",
@@ -53,10 +53,10 @@
         "proj_conf_record": "5.0-15.0",
         "projected_losses": "19.5",
         "projected_wins": "11.5",
-        "rank": "108"
+        "rank": "107"
     },
     "time": {
-        "time": "Dec 06,  9:30 PM"
+        "time": "Dec 07, 10:31 AM"
     },
     "trank": {
         "trank": "112",
@@ -69,13 +69,13 @@
     "warrennolan": {
         "current_conf_record": "0-1",
         "elo": "182",
-        "nolan_net": "192",
+        "nolan_net": "191",
         "quadrant_1": "0-1",
-        "quadrant_2": "0-0",
-        "quadrant_3": "0-3",
+        "quadrant_2": "0-1",
+        "quadrant_3": "0-2",
         "quadrant_4": "5-0",
         "record": "5-4",
-        "rpi": "245",
-        "sos": "247"
+        "rpi": "246",
+        "sos": "248"
     }
 }
