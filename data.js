91f143a01b0586ce4f189e2a4109fe808c9483c3
Ben Vankat <bvankat@gmail.com>
2022-01-05 00:30:27 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -2,80 +2,80 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "38"
+        "total_brackets": "46"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "154",
+        "bpi": "162",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "4.0-16.0",
-        "proj_win_loss_overall": "10.0-21.0",
-        "seven_day_change": "-3",
-        "sor": "225",
-        "sos": "148"
+        "proj_win_loss_conf": "3.6-16.4",
+        "proj_win_loss_overall": "9.6-21.4",
+        "seven_day_change": "-8",
+        "sor": "240",
+        "sos": "117"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "138"
+        "miya_rating": "135"
     },
     "haslametrics": {
         "haslam_rating": "141"
     },
     "kenpom": {
-        "conference_record": "4-16",
-        "overall_record": "10-21",
-        "rating": "136",
-        "record": "6-7",
-        "sos": "202"
+        "conference_record": "3-17",
+        "overall_record": "9-22",
+        "rating": "141",
+        "record": "6-8",
+        "sos": "162"
     },
     "kpi_sports": {
-        "kpi_ranking": 201
+        "kpi_ranking": 214
     },
     "ncaa": {
-        "net_rank": "221"
+        "net_rank": "217"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "139"
+        "sagarin_rate": "138"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
         "most_likely_seed": "13",
-        "proj_conf_record": "3.4-16.6",
-        "projected_losses": "21.6",
-        "projected_wins": "9.4",
-        "rank": "145"
+        "proj_conf_record": "3.3-16.7",
+        "projected_losses": "21.7",
+        "projected_wins": "9.3",
+        "rank": "142"
     },
     "time": {
-        "time": "Dec 28,  9:30 AM"
+        "time": "Jan 05, 12:30 AM"
     },
     "trank": {
-        "trank": "172",
+        "trank": "165",
         "trank_make_tourney": "0",
         "trank_proj_record": "9-22 (3-17)",
         "trank_seed": "NR",
-        "wab_rank": "246",
-        "wab_score": "-3.7246"
+        "wab_rank": "253",
+        "wab_score": "-4.2253"
     },
     "warrennolan": {
-        "current_conf_record": "0-2",
-        "elo": "201",
-        "nolan_net": "221",
-        "quadrant_1": "0-2",
+        "current_conf_record": "0-3",
+        "elo": "203",
+        "nolan_net": "224",
+        "quadrant_1": "0-3",
         "quadrant_2": "0-2",
-        "quadrant_3": "0-3",
-        "quadrant_4": "6-0",
-        "record": "6-7",
-        "rpi": "257",
-        "sos": "190"
+        "quadrant_3": "0-2",
+        "quadrant_4": "6-1",
+        "record": "6-8",
+        "rpi": "258",
+        "sos": "162"
     }
 }
