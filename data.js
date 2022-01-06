c20c3dbf44af76787d7410b735897d2fe9749c85
Ben Vankat <bvankat@gmail.com>
2022-01-06 12:30:50 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -8,24 +8,24 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "161",
+        "bpi": "158",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "3.6-16.4",
         "proj_win_loss_overall": "9.6-21.4",
-        "seven_day_change": "-8",
-        "sor": "237",
-        "sos": "115"
+        "seven_day_change": "-5",
+        "sor": "244",
+        "sos": "74"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "136"
+        "miya_rating": "135"
     },
     "haslametrics": {
-        "haslam_rating": "141"
+        "haslam_rating": "145"
     },
     "kenpom": {
         "conference_record": "3-17",
@@ -35,41 +35,41 @@
         "sos": "124"
     },
     "kpi_sports": {
-        "kpi_ranking": 218
+        "kpi_ranking": 210
     },
     "ncaa": {
-        "net_rank": "217"
+        "net_rank": "206"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "137"
+        "sagarin_rate": "133"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
-        "most_likely_seed": "13",
+        "most_likely_seed": "14",
         "proj_conf_record": "3.3-16.7",
         "projected_losses": "21.7",
         "projected_wins": "9.3",
         "rank": "142"
     },
     "time": {
-        "time": "Jan 05, 10:30 PM"
+        "time": "Jan 06, 12:30 PM"
     },
     "trank": {
-        "trank": "162",
+        "trank": "161",
         "trank_make_tourney": "0",
         "trank_proj_record": "9-22 (3-17)",
         "trank_seed": "NR",
-        "wab_rank": "266",
-        "wab_score": "-4.5266"
+        "wab_rank": "265",
+        "wab_score": "-4.4265"
     },
     "warrennolan": {
         "current_conf_record": "0-4",
         "elo": "206",
-        "nolan_net": "217",
+        "nolan_net": "206",
         "quadrant_1": "0-4",
         "quadrant_2": "0-2",
         "quadrant_3": "0-2",
