6ca67b4d00d7741a01ae40525b84caefce45e9d2
Ben Vankat <bvankat@gmail.com>
2022-01-21 22:31:14 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -2,21 +2,21 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "88"
+        "total_brackets": "93"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "164",
+        "bpi": "163",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "2.6-17.4",
-        "proj_win_loss_overall": "8.6-22.4",
-        "seven_day_change": "3",
-        "sor": "236",
-        "sos": "15"
+        "proj_win_loss_conf": "2.5-16.5",
+        "proj_win_loss_overall": "8.5-21.5",
+        "seven_day_change": "7",
+        "sor": "233",
+        "sos": "17"
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -32,13 +32,13 @@
         "overall_record": "8-22",
         "rating": "163",
         "record": "6-13",
-        "sos": "20"
+        "sos": "24"
     },
     "kpi_sports": {
-        "kpi_ranking": 190
+        "kpi_ranking": 187
     },
     "ncaa": {
-        "net_rank": "200"
+        "net_rank": "198"
     },
     "polls": {
         "ap_rank": "NR",
@@ -56,26 +56,26 @@
         "rank": "145"
     },
     "time": {
-        "time": "Jan 21,  6:33 AM"
+        "time": "Jan 21, 10:31 PM"
     },
     "trank": {
-        "trank": "158",
+        "trank": "159",
         "trank_make_tourney": "0",
         "trank_proj_record": "9-21 (3-16)",
         "trank_seed": "NR",
-        "wab_rank": "253",
-        "wab_score": "-6"
+        "wab_rank": "256",
+        "wab_score": "-6.1"
     },
     "warrennolan": {
         "current_conf_record": "0-8",
-        "elo": "223",
-        "nolan_net": "200",
+        "elo": "224",
+        "nolan_net": "198",
         "quadrant_1": "0-6",
         "quadrant_2": "0-6",
         "quadrant_3": "0-0",
         "quadrant_4": "6-1",
         "record": "6-13",
-        "rpi": "205",
+        "rpi": "206",
         "sos": "39"
     }
 }
