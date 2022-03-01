bb4a72163a5abd9fdc4b99a3bee3b75b53559b21
Ben Vankat <bvankat@gmail.com>
2022-03-01 10:30:55 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -2,27 +2,27 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "106"
+        "total_brackets": "119"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "160",
+        "bpi": "162",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "2.1-17.9",
         "proj_win_loss_overall": "8.1-22.9",
-        "seven_day_change": "15",
-        "sor": "202",
-        "sos": "34"
+        "seven_day_change": "11",
+        "sor": "205",
+        "sos": "37"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "150"
+        "miya_rating": "156"
     },
     "haslametrics": {
         "haslam_rating": "131"
@@ -32,13 +32,13 @@
         "overall_record": "8-23",
         "rating": "155",
         "record": "8-21",
-        "sos": "31"
+        "sos": "33"
     },
     "kpi_sports": {
         "kpi_ranking": 192
     },
     "ncaa": {
-        "net_rank": "164"
+        "net_rank": "166"
     },
     "polls": {
         "ap_rank": "NR",
@@ -56,20 +56,20 @@
         "rank": "135"
     },
     "time": {
-        "time": "Feb 28,  8:30 PM"
+        "time": "Mar 01, 10:30 AM"
     },
     "trank": {
         "trank": "140",
         "trank_make_tourney": "0",
         "trank_proj_record": "8-23 (2-18)",
         "trank_seed": "NR",
-        "wab_rank": "243",
-        "wab_score": "-9.8"
+        "wab_rank": "240",
+        "wab_score": "-9.7"
     },
     "warrennolan": {
         "current_conf_record": "2-16",
         "elo": "213",
-        "nolan_net": "164",
+        "nolan_net": "166",
         "quadrant_1": "0-10",
         "quadrant_2": "1-6",
         "quadrant_3": "1-4",
