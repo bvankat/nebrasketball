3b5693ea3a0fb416e526d08687612698651d6a7a
Ben Vankat <bvankat@gmail.com>
2022-03-04 10:30:28 -0600

    Latest automated update.

--- data.js
+++ data.js
@@ -2,30 +2,30 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "130"
+        "total_brackets": "112"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "147",
+        "bpi": "148",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
         "proj_win_loss_conf": "3.1-16.9",
         "proj_win_loss_overall": "9.1-21.9",
-        "seven_day_change": "25",
-        "sor": "185",
-        "sos": "31"
+        "seven_day_change": "24",
+        "sor": "186",
+        "sos": "32"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "146"
+        "miya_rating": "147"
     },
     "haslametrics": {
-        "haslam_rating": "120"
+        "haslam_rating": "118"
     },
     "kenpom": {
         "conference_record": "3-17",
@@ -35,17 +35,17 @@
         "sos": "28"
     },
     "kpi_sports": {
-        "kpi_ranking": 168
+        "kpi_ranking": 169
     },
     "ncaa": {
-        "net_rank": "148"
+        "net_rank": "147"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "119"
+        "sagarin_rate": "117"
     },
     "teamrankings": {
         "make_tournament": "0.0%",
@@ -53,29 +53,29 @@
         "proj_conf_record": "3.1-16.9",
         "projected_losses": "21.9",
         "projected_wins": "9.1",
-        "rank": "130"
+        "rank": "129"
     },
     "time": {
-        "time": "Mar 03, 12:31 PM"
+        "time": "Mar 04, 10:30 AM"
     },
     "trank": {
-        "trank": "129",
-        "trank_make_tourney": "0.1",
+        "trank": "128",
+        "trank_make_tourney": "0",
         "trank_proj_record": "9-22 (3-17)",
         "trank_seed": "NR",
-        "wab_rank": "227",
-        "wab_score": "-9"
+        "wab_rank": "224",
+        "wab_score": "-8.9"
     },
     "warrennolan": {
         "current_conf_record": "3-16",
-        "elo": "192",
-        "nolan_net": "148",
+        "elo": "187",
+        "nolan_net": "147",
         "quadrant_1": "1-11",
         "quadrant_2": "1-6",
         "quadrant_3": "1-3",
         "quadrant_4": "6-1",
         "record": "55",
-        "rpi": "220",
-        "sos": "226"
+        "rpi": "222",
+        "sos": "234"
     }
 }
