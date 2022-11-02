356f7faeaa7baa2be8613fba614f41cb39b77c29
Benjamin Vankat <bvankat@gmail.com>
2022-11-02 06:39:30 +0000

    Initial push

---
+++ updates-2023.js
@@ -0,0 +1,51 @@
+{
+    "updates": [
+        {
+            "kenpom": {
+                "rating": "108",
+                "record": "0-0",
+                "sos": "1",
+                "overall_record": "10-19",
+                "conference_record": "5-15"
+            },
+            "polls": {
+                "ap_rank": "NR",
+                "coaches_rank": "NR"
+            },
+            "teamrankings": {
+                "make_tournament": "13.2%",
+                "most_likely_seed": "11",
+                "rank": "80",
+                "projected_wins": "12.5",
+                "projected_losses": "16.5",
+                "proj_conf_record": "6.4-13.6"
+            },
+            "espn_bracketology": {
+                "seed": "NR"
+            },
+            "trank": {
+                "trank": "92",
+                "trank_proj_record": "13-16 (8-12)",
+                "trank_seed": "NR",
+                "trank_make_tourney": "1.5",
+                "wab_score": "NA",
+                "wab_rank": "NA"
+            },
+            "cbs": {
+                "cbs_rank": "NR"
+            },
+            "sagarin": {
+                "sagarin_rate": "90"
+            },
+            "haslametrics": {
+                "haslam_rating": "120"
+            },
+            "evanmiya": {
+                "miya_rating": "98"
+            },
+            "time": {
+                "time": "Nov 02,  1:34 AM"
+            }
+        }
+    ]
+}
