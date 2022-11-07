d62672883e77564917c1bb5e5c8f976ebc867a03
Benjamin Vankat <bvankat@gmail.com>
2022-11-07 17:09:34 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,6 +1,47 @@
 {
+    "cbs": {
+        "cbs_rank": "NR"
+    },
+    "espn": {
+        "bpi": "83",
+        "chance_round_32": "0.5%",
+        "chance_sweet_16": "0.2%",
+        "proj_tourney_seed": "NR",
+        "proj_win_loss_conf": "8.3-11.7",
+        "proj_win_loss_overall": "13.9-15.1",
+        "seven_day_change": "",
+        "sor": "4",
+        "sos": "5"
+    },
+    "espn_bracketology": {
+        "seed": "NR"
+    },
+    "haslametrics": {
+        "haslam_rating": "120"
+    },
+    "kenpom": {
+        "conference_record": "5-15",
+        "overall_record": "11-18",
+        "rating": "108",
+        "record": "0-0",
+        "sos": "5"
+    },
+    "ncaa": {
+        "net_rank": "145"
+    },
+    "sagarin": {
+        "sagarin_rate": "90"
+    },
+    "teamrankings": {
+        "make_tournament": "10.3%",
+        "most_likely_seed": "12",
+        "proj_conf_record": "6.1-13.9",
+        "projected_losses": "17.7",
+        "projected_wins": "11.3",
+        "rank": "102"
+    },
     "time": {
-        "time": "Nov 07,  4:08 PM"
+        "time": "Nov 07,  5:09 PM"
     },
     "trank": {
         "trank": "91",
@@ -9,5 +50,16 @@
         "trank_seed": "NR",
         "wab_rank": "3",
         "wab_score": "0"
+    },
+    "warrennolan": {
+        "current_conf_record": "0-0",
+        "elo": "164",
+        "nolan_net": "145",
+        "quadrant_1": "0-0",
+        "quadrant_2": "0-0",
+        "quadrant_3": "0-0",
+        "quadrant_4": "0-0",
+        "record": "145",
+        "rpi": ""
     }
 }
--- data/updates-2023.js
+++ data/updates-2023.js
@@ -374,6 +374,71 @@
             "time": {
                 "time": "Nov 07,  4:08 PM"
             }
+        },
+        {
+            "kenpom": {
+                "rating": "108",
+                "record": "0-0",
+                "sos": "5",
+                "overall_record": "11-18",
+                "conference_record": "5-15"
+            },
+            "ncaa": {
+                "net_rank": "145"
+            },
+            "teamrankings": {
+                "make_tournament": "10.3%",
+                "most_likely_seed": "12",
+                "rank": "102",
+                "projected_wins": "11.3",
+                "projected_losses": "17.7",
+                "proj_conf_record": "6.1-13.9"
+            },
+            "espn": {
+                "bpi": "83",
+                "seven_day_change": "",
+                "proj_tourney_seed": "NR",
+                "proj_win_loss_overall": "13.9-15.1",
+                "proj_win_loss_conf": "8.3-11.7",
+                "sor": "4",
+                "sos": "5",
+                "chance_sweet_16": "0.2%",
+                "chance_round_32": "0.5%"
+            },
+            "espn_bracketology": {
+                "seed": "NR"
+            },
+            "warrennolan": {
+                "record": "145",
+                "current_conf_record": "0-0",
+                "rpi": "",
+                "elo": "164",
+                "quadrant_1": "0-0",
+                "quadrant_2": "0-0",
+                "quadrant_3": "0-0",
+                "quadrant_4": "0-0",
+                "nolan_net": "145"
+            },
+            "trank": {
+                "trank": "91",
+                "trank_proj_record": "13-16 (8-12)",
+                "trank_seed": "NR",
+                "trank_make_tourney": "1.3",
+                "wab_score": "0",
+                "wab_rank": "3"
+            },
+            "cbs": {
+                "cbs_rank": "NR"
+            },
+            "sagarin": {
+                "sagarin_rate": "90"
+            },
+            "haslametrics": {
+                "haslam_rating": "120"
+            },
+            "time": {
+                "time": "Nov 07,  5:09 PM"
+            }
         }
     ]
 }
