a889d3480d2b18acc94857dce3f132913a278e44
Benjamin Vankat <bvankat@gmail.com>
2022-11-09 11:37:34 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,13 +1,69 @@
 {
-    "bracketproject": {
-        "matrix_count": 0,
-        "matrix_seed": "NR",
-        "total_brackets": "47"
+    "cbs": {
+        "cbs_rank": "NR"
     },
-    "evanmiya": {
-        "miya_rating": "105"
+    "espn": {
+        "bpi": "85",
+        "chance_round_32": "0.4%",
+        "chance_sweet_16": "0.2%",
+        "proj_tourney_seed": "NR",
+        "proj_win_loss_conf": "8.1-11.9",
+        "proj_win_loss_overall": "13.7-15.3",
+        "seven_day_change": "-2",
+        "sor": "114",
+        "sos": "245"
+    },
+    "espn_bracketology": {
+        "seed": "NR"
+    },
+    "haslametrics": {
+        "haslam_rating": "120"
+    },
+    "kenpom": {
+        "conference_record": "5-15",
+        "overall_record": "10-19",
+        "rating": "114",
+        "record": "1-0",
+        "sos": "255"
+    },
+    "ncaa": {
+        "net_rank": "145"
+    },
+    "polls": {
+        "ap_rank": "NR",
+        "coaches_rank": "NR"
+    },
+    "sagarin": {
+        "sagarin_rate": "93"
+    },
+    "teamrankings": {
+        "make_tournament": "7.3%",
+        "most_likely_seed": "12",
+        "proj_conf_record": "5.4-14.6",
+        "projected_losses": "18.5",
+        "projected_wins": "10.5",
+        "rank": "111"
     },
     "time": {
-        "time": "Nov 09, 10:29 AM"
+        "time": "Nov 09, 11:37 AM"
+    },
+    "trank": {
+        "trank": "94",
+        "trank_make_tourney": "0.9",
+        "trank_proj_record": "12-17 (7-13)",
+        "trank_seed": "NR",
+        "wab_rank": "108",
+        "wab_score": "0"
+    },
+    "warrennolan": {
+        "current_conf_record": "0-0",
+        "elo": "152",
+        "nolan_net": "145",
+        "quadrant_1": "0-0",
+        "quadrant_2": "0-0",
+        "quadrant_3": "0-0",
+        "quadrant_4": "1-0",
+        "record": "145",
+        "rpi": "1"
     }
 }
