9a910dd2240bd815000ad7516189219446815b0d
Benjamin Vankat <bvankat@gmail.com>
2022-11-28 09:26:01 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -22,7 +22,7 @@
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "115"
+        "miya_rating": "118"
     },
     "haslametrics": {
         "haslam_rating": "131"
@@ -42,25 +42,25 @@
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "119"
+        "sagarin_rate": "98"
     },
     "shot_quality": {
-        "sq_ranking": 65
+        "sq_ranking": 67
     },
     "teamrankings": {
-        "make_tournament": "2.8%",
+        "make_tournament": "5.8%",
         "most_likely_seed": "12",
-        "proj_conf_record": "4.5-15.5",
-        "projected_losses": "21.3",
-        "projected_wins": "9.7",
-        "rank": "122"
+        "proj_conf_record": "4.9-15.1",
+        "projected_losses": "20.3",
+        "projected_wins": "10.7",
+        "rank": "105"
     },
     "time": {
-        "time": "Nov 28,  1:14 AM"
+        "time": "Nov 28,  9:26 AM"
     },
     "trank": {
-        "trank": "101",
-        "trank_make_tourney": "0.1",
+        "trank": "105",
+        "trank_make_tourney": "0.3",
         "trank_proj_record": "12-19 (6-14)",
         "trank_seed": "NR",
         "wab_rank": "130",
