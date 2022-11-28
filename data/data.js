ee33fb3030c68297e31ffcdae3825da0b55ac169
Benjamin Vankat <bvankat@gmail.com>
2022-11-28 01:14:57 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -22,17 +22,17 @@
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "116"
+        "miya_rating": "115"
     },
     "haslametrics": {
-        "haslam_rating": "141"
+        "haslam_rating": "131"
     },
     "kenpom": {
         "conference_record": "5-15",
-        "overall_record": "10-21",
-        "rating": "120",
-        "record": "3-3",
-        "sos": "185"
+        "overall_record": "11-20",
+        "rating": "111",
+        "record": "4-3",
+        "sos": "182"
     },
     "ncaa": {
         "net_rank": "145"
@@ -45,7 +45,7 @@
         "sagarin_rate": "119"
     },
     "shot_quality": {
-        "sq_ranking": 54
+        "sq_ranking": 65
     },
     "teamrankings": {
         "make_tournament": "2.8%",
@@ -56,25 +56,25 @@
         "rank": "122"
     },
     "time": {
-        "time": "Nov 27, 12:10 PM"
+        "time": "Nov 28,  1:14 AM"
     },
     "trank": {
-        "trank": "113",
+        "trank": "101",
         "trank_make_tourney": "0.1",
-        "trank_proj_record": "11-20 (6-14)",
+        "trank_proj_record": "12-19 (6-14)",
         "trank_seed": "NR",
-        "wab_rank": "157",
-        "wab_score": "-1.1"
+        "wab_rank": "130",
+        "wab_score": "-0.9"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "elo": "180",
+        "elo": "161",
         "nolan_net": "145",
         "quadrant_1": "0-3",
         "quadrant_2": "0-0",
-        "quadrant_3": "0-0",
+        "quadrant_3": "1-0",
         "quadrant_4": "3-0",
         "record": "145",
-        "rpi": "102"
+        "rpi": "127"
     }
 }
