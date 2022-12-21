4d2b11506eff8da1a832d365c1c88e326880ab27
Benjamin Vankat <bvankat@gmail.com>
2022-12-21 15:44:01 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -2,85 +2,78 @@
     "bracketproject": {
         "matrix_count": 0,
         "matrix_seed": "NR",
-        "total_brackets": "27"
+        "total_brackets": "38"
     },
     "cbs": {
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": "93",
+        "bpi": "99",
         "chance_round_32": "0.0%",
         "chance_sweet_16": "0.0%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "6.5-13.5",
-        "proj_win_loss_overall": "13.4-17.6",
-        "seven_day_change": "-2",
-        "sor": "102",
-        "sos": "7"
+        "proj_win_loss_conf": "6.1-13.9",
+        "proj_win_loss_overall": "13.1-17.9",
+        "seven_day_change": "-6",
+        "sor": "89",
+        "sos": "8"
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "95"
+        "miya_rating": "96"
     },
     "haslametrics": {
-        "haslam_rating": "91"
-    },
-    "kenpom": {
-        "conference_record": "6-14",
-        "overall_record": "13-18",
-        "rating": "83",
-        "record": "6-6",
-        "sos": "23"
+        "haslam_rating": "99"
     },
     "kpi_sports": {
-        "kpi_ranking": 95
+        "kpi_ranking": 86
     },
     "massey_composite": {
-        "massey_rank": "76"
+        "massey_rank": "91"
     },
     "ncaa": {
-        "net_rank": "80"
+        "net_rank": "81"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "84"
+        "sagarin_rate": "91"
     },
     "shot_quality": {
-        "sq_ranking": 47
+        "sq_ranking": 61
     },
     "teamrankings": {
-        "make_tournament": "7.5%",
+        "make_tournament": "6.5%",
         "most_likely_seed": "12",
-        "proj_conf_record": "5.8-14.2",
+        "proj_conf_record": "5.7-14.3",
         "projected_losses": "18.3",
         "projected_wins": "12.7",
-        "rank": "84"
+        "rank": "86"
     },
     "time": {
-        "time": "Dec. 18, 2022 at 2:41 PM"
+        "time": "Dec. 21, 2022 at 3:44 PM"
     },
     "trank": {
-        "trank": "98",
-        "trank_make_tourney": "0.4",
+        "trank": "94",
+        "trank_make_tourney": "0.3",
         "trank_proj_record": "13-18 (6-14)",
         "trank_seed": "NR",
-        "wab_rank": "125",
+        "wab_rank": "115",
         "wab_score": "-1.5"
     },
     "warrennolan": {
         "current_conf_record": "0-2",
-        "elo": "135",
-        "nolan_net": "80",
-        "quadrant_1": "1-5",
-        "quadrant_2": "0-1",
-        "quadrant_3": "0-0",
+        "elo": "119",
+        "nolan_net": "81",
+        "quadrant_1": "1-6",
+        "quadrant_2": "0-0",
+        "quadrant_3": "1-0",
         "quadrant_4": "5-0",
-        "record": "80",
-        "rpi": "96"
+        "record": "81",
+        "rpi": "71"
     }
 }
--- data/updates-2023.js
+++ data/updates-2023.js
@@ -5817,6 +5817,85 @@
             "time": {
                 "time": "Dec. 18, 2022 at 2:41 PM"
             }
+        },
+        {
+            "evanmiya": {
+                "miya_rating": "96"
+            },
+            "ncaa": {
+                "net_rank": "81"
+            },
+            "polls": {
+                "ap_rank": "NR",
+                "coaches_rank": "NR"
+            },
+            "teamrankings": {
+                "make_tournament": "6.5%",
+                "most_likely_seed": "12",
+                "rank": "86",
+                "projected_wins": "12.7",
+                "projected_losses": "18.3",
+                "proj_conf_record": "5.7-14.3"
+            },
+            "espn": {
+                "bpi": "99",
+                "seven_day_change": "-6",
+                "proj_tourney_seed": "NR",
+                "proj_win_loss_overall": "13.1-17.9",
+                "proj_win_loss_conf": "6.1-13.9",
+                "sor": "89",
+                "sos": "8",
+                "chance_sweet_16": "0.0%",
+                "chance_round_32": "0.0%"
+            },
+            "espn_bracketology": {
+                "seed": "NR"
+            },
+            "warrennolan": {
+                "record": "81",
+                "current_conf_record": "0-2",
+                "rpi": "71",
+                "elo": "119",
+                "quadrant_1": "1-6",
+                "quadrant_2": "0-0",
+                "quadrant_3": "1-0",
+                "quadrant_4": "5-0",
+                "nolan_net": "81"
+            },
+            "trank": {
+                "trank": "94",
+                "trank_proj_record": "13-18 (6-14)",
+                "trank_seed": "NR",
+                "trank_make_tourney": "0.3",
+                "wab_score": "-1.5",
+                "wab_rank": "115"
+            },
+            "cbs": {
+                "cbs_rank": "NR"
+            },
+            "sagarin": {
+                "sagarin_rate": "91"
+            },
+            "haslametrics": {
+                "haslam_rating": "99"
+            },
+            "kpi_sports": {
+                "kpi_ranking": 86
+            },
+            "shot_quality": {
+                "sq_ranking": 61
+            },
+            "bracketproject": {
+                "matrix_count": 0,
+                "total_brackets": "38",
+                "matrix_seed": "NR"
+            },
+            "massey_composite": {
+                "massey_rank": "91"
+            },
+            "time": {
+                "time": "Dec. 21, 2022 at 3:44 PM"
+            }
         }
     ]
 }
