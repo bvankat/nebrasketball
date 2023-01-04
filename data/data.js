1bebece6a1b7605f01e20a38a4a7d7c3172d1063
Benjamin Vankat <bvankat@gmail.com>
2023-01-04 10:13:37 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -22,7 +22,7 @@
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "89"
+        "miya_rating": "88"
     },
     "haslametrics": {
         "haslam_rating": "95"
@@ -35,38 +35,38 @@
         "sos": "12"
     },
     "kpi_sports": {
-        "kpi_ranking": 74
+        "kpi_ranking": 76
     },
     "massey_composite": {
         "massey_rank": "81"
     },
     "ncaa": {
-        "net_rank": "78"
+        "net_rank": "84"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "sagarin": {
-        "sagarin_rate": "82"
+        "sagarin_rate": "87"
     },
     "shot_quality": {
-        "sq_ranking": 67
+        "sq_ranking": 64
     },
     "teamrankings": {
-        "make_tournament": "14.4%",
+        "make_tournament": "10.0%",
         "most_likely_seed": "12",
-        "proj_conf_record": "7.2-12.8",
-        "projected_losses": "16.8",
-        "projected_wins": "14.2",
-        "rank": "82"
+        "proj_conf_record": "6.7-13.3",
+        "projected_losses": "17.3",
+        "projected_wins": "13.7",
+        "rank": "87"
     },
     "time": {
-        "time": "Jan. 4, 2023 at 1:31 AM"
+        "time": "Jan. 4, 2023 at 10:13 AM"
     },
     "trank": {
         "trank": "93",
-        "trank_make_tourney": "3.1",
+        "trank_make_tourney": "1.1",
         "trank_proj_record": "14-17 (7-13)",
         "trank_seed": "NR",
         "wab_rank": "106",
@@ -75,12 +75,12 @@
     "warrennolan": {
         "current_conf_record": "1-3",
         "elo": "113",
-        "nolan_net": "78",
-        "quadrant_1": "1-5",
-        "quadrant_2": "1-2",
+        "nolan_net": "84",
+        "quadrant_1": "1-4",
+        "quadrant_2": "1-3",
         "quadrant_3": "1-0",
         "quadrant_4": "5-0",
-        "record": "78",
+        "record": "84",
         "rpi": "79"
     }
 }
