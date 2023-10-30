06a0f3235915089ebcccffab503d73ac5c7bf686
Benjamin Vankat <bvankat@gmail.com>
2023-10-30 13:00:01 -0500

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -3,7 +3,10 @@
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "68"
+        "miya_rating": "65"
+    },
+    "haslametrics": {
+        "haslam_rating": "78"
     },
     "kenpom": {
         "conference_record": "9-11",
@@ -17,14 +20,25 @@
         "coaches_rank": "NR"
     },
     "time": {
-        "time": "Oct. 23, 2023 at 10:31 AM"
+        "time": "Oct. 30, 2023 at 1:00 PM"
     },
     "trank": {
         "trank": "47",
-        "trank_make_tourney": "46.6",
+        "trank_make_tourney": "47.6",
         "trank_proj_record": "19-12 (10-10)",
         "trank_seed": "11",
         "wab_rank": "NA",
         "wab_score": "NA"
+    },
+    "warrennolan": {
+        "current_conf_record": "0-0",
+        "elo": "99",
+        "nolan_net": "92",
+        "quadrant_1": "0-0",
+        "quadrant_2": "0-0",
+        "quadrant_3": "0-0",
+        "quadrant_4": "0-0",
+        "record": "92",
+        "rpi": "1"
     }
 }
