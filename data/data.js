05df34af9179d572ab3373652c0962d1a1bb3dfd
Benjamin Vankat <bvankat@gmail.com>
2023-11-17 09:34:30 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -8,32 +8,32 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": 51.0,
-        "chance_round_32": "9.0%",
-        "chance_sweet_16": "3.7%",
+        "bpi": 50.0,
+        "chance_round_32": "9.7%",
+        "chance_sweet_16": "3.8%",
         "proj_tourney_seed": "NR",
-        "proj_win_loss_conf": "9.8-10.2",
-        "proj_win_loss_overall": "18.8-12.2",
-        "projected_tournament_order": "75",
+        "proj_win_loss_conf": "9.7-10.3",
+        "proj_win_loss_overall": "18.9-12.1",
+        "projected_tournament_order": "81",
         "seven_day_change": "1st",
-        "sor": 69.0,
-        "sos": 335.0
+        "sor": 75.0,
+        "sos": 342.0
     },
     "espn_bracketology": {
         "seed": "NR"
     },
     "evanmiya": {
-        "miya_rating": "50"
+        "miya_rating": "49"
     },
     "haslametrics": {
-        "haslam_rating": "68"
+        "haslam_rating": "61"
     },
     "kenpom": {
         "conference_record": "10-10",
         "overall_record": "19-12",
-        "rating": "54",
+        "rating": "53",
         "record": "4-0",
-        "sos": "347"
+        "sos": "351"
     },
     "kpi_sports": {
         "kpi_ranking": null
@@ -47,41 +47,41 @@
     },
     "seven_overtimes": {
         "projected_conf_record": "11-9",
-        "projected_record": "20-11",
-        "ranking": "80"
+        "projected_record": "20-12",
+        "ranking": "75"
     },
     "shot_quality": {
-        "sq_ranking": 203
+        "sq_ranking": 201
     },
     "teamrankings": {
-        "make_tournament": "13.4%",
+        "make_tournament": "12.6%",
         "most_likely_seed": "8",
-        "proj_conf_record": "8.9-11.1",
-        "projected_losses": "13.1",
-        "projected_wins": "17.9",
-        "rank": "58"
+        "proj_conf_record": "9.0-11.0",
+        "projected_losses": "13.0",
+        "projected_wins": "18.0",
+        "rank": "57"
     },
     "time": {
-        "time": "Nov. 16, 2023 at 2:56 PM"
+        "time": "Nov. 17, 2023 at 9:34 AM"
     },
     "trank": {
-        "torvik_NET": "40",
-        "trank": "41",
-        "trank_make_tourney": "61.7",
+        "torvik_NET": "41",
+        "trank": "40",
+        "trank_make_tourney": "61.5",
         "trank_proj_record": "20-11 (10-10)",
         "trank_seed": "11",
-        "wab_rank": "56",
+        "wab_rank": "60",
         "wab_score": "0.2"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "elo": "81",
+        "elo": "82",
         "nolan_net": "92",
         "quadrant_1": "0-0",
         "quadrant_2": "0-0",
         "quadrant_3": "0-0",
         "quadrant_4": "4-0",
         "record": "4-0",
-        "rpi": "192"
+        "rpi": "208"
     }
 }
