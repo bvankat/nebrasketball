cef1e920865f9d7992760f4a775a9222becb6ed0
Benjamin Vankat <bvankat@gmail.com>
2023-12-04 09:24:02 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -8,16 +8,16 @@
         "cbs_rank": "NR"
     },
     "espn": {
-        "bpi": 48.0,
-        "chance_round_32": "8.0%",
-        "chance_sweet_16": "2.8%",
-        "proj_tourney_seed": "10",
-        "proj_win_loss_conf": "9.9-10.1",
-        "proj_win_loss_overall": "19.5-11.5",
-        "projected_tournament_order": "40",
-        "seven_day_change": "Tied-2nd",
-        "sor": 41.0,
-        "sos": 360.0
+        "bpi": 64.0,
+        "chance_round_32": "2.2%",
+        "chance_sweet_16": "1.0%",
+        "proj_tourney_seed": "13",
+        "proj_win_loss_conf": "9.0-11.0",
+        "proj_win_loss_overall": "18.3-12.7",
+        "projected_tournament_order": "51",
+        "seven_day_change": "Tied-17th",
+        "sor": 52.0,
+        "sos": 300.0
     },
     "espn_bracketology": {
         "seed": "NR"
@@ -26,64 +26,64 @@
         "miya_rating": "46"
     },
     "haslametrics": {
-        "haslam_rating": "54"
+        "haslam_rating": "67"
     },
     "kenpom": {
         "conference_record": "9-11",
         "overall_record": "18-13",
         "rating": "58",
         "record": "7-1",
-        "sos": "338"
+        "sos": "337"
     },
     "kpi_sports": {
-        "kpi_ranking": null
+        "kpi_ranking": 59
     },
-    "massey_ratings": {
-        "massey_rank": "45"
+    "ncaa": {
+        "net_rank": "76"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "seven_overtimes": {
-        "projected_conf_record": "11-9",
+        "projected_conf_record": "12-8",
         "projected_record": "21-10",
-        "ranking": "19"
+        "ranking": "43"
     },
     "shot_quality": {
-        "sq_ranking": 76
+        "sq_ranking": 73
     },
     "sports_reference": {
         "SRS_rank": "48"
     },
     "teamrankings": {
-        "make_tournament": "16.9%",
-        "most_likely_seed": "7",
-        "proj_conf_record": "9.5-10.5",
-        "projected_losses": "11.8",
-        "projected_wins": "19.2",
-        "rank": "55"
+        "make_tournament": "6.9%",
+        "most_likely_seed": "8",
+        "proj_conf_record": "8.3-11.7",
+        "projected_losses": "13.4",
+        "projected_wins": "17.6",
+        "rank": "67"
     },
     "time": {
-        "time": "Dec. 3, 2023 at 11:04 PM"
+        "time": "Dec. 4, 2023 at 9:24 AM"
     },
     "trank": {
-        "torvik_NET": "42",
+        "torvik_NET": "56",
         "trank": "39",
-        "trank_make_tourney": "84",
+        "trank_make_tourney": "63.1",
         "trank_proj_record": "20-11 (11-9)",
-        "trank_seed": "10",
+        "trank_seed": "NR",
         "wab_rank": "35",
         "wab_score": "0.6"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
         "elo": "62",
-        "nolan_net": "92",
+        "nolan_net": "76",
         "quadrant_1": "0-1",
         "quadrant_2": "0-0",
-        "quadrant_3": "2-0",
-        "quadrant_4": "5-0",
+        "quadrant_3": "1-0",
+        "quadrant_4": "6-0",
         "record": "7-1",
         "rpi": "118"
     }
