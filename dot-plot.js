f072af93d1a5071b4b64a0380d340ef176a8a0f7
Benjamin Vankat <bvankat@gmail.com>
2024-01-05 10:11:20 -0600

    Color changes

--- dot-plot.js
+++ dot-plot.js
@@ -38,7 +38,7 @@ const highestEndpoint = highestDataPoint + 15;
 
 // Color scale for dots
 const colorScale = d3.scaleLinear()
-  .domain([highestEndpoint, 40, 10])
+  .domain([highestEndpoint, 40, 20])
   .range(['#ffffcc', '#fea849', '#e42320']);
 
 // Create an SVG container
