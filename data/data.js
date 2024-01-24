c2254f8ece1937e0522e86f9a7eed0b9755761c4
Benjamin Vankat <bvankat@gmail.com>
2024-01-24 10:18:54 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -24,61 +24,61 @@
         "seed": "9"
     },
     "evanmiya": {
-        "miya_rating": "59"
+        "miya_rating": "51"
     },
     "haslametrics": {
-        "haslam_rating": "45",
-        "seed": "9"
+        "haslam_rating": "41",
+        "seed": "7"
     },
     "kenpom": {
         "conference_record": "11-9",
         "overall_record": "21-10",
         "rating": "45",
         "record": "15-5",
-        "sos": "75"
+        "sos": "76"
     },
     "kpi_sports": {
-        "kpi_ranking": 31
+        "kpi_ranking": 24
     },
     "massey_ratings": {
         "massey_rank": "44"
     },
     "ncaa": {
-        "net_rank": "56"
+        "net_rank": "47"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "seven_overtimes": {
-        "projected_conf_record": "11-9",
-        "projected_record": "21-10",
+        "projected_conf_record": "12-8",
+        "projected_record": "22-9",
         "ranking": "46"
     },
     "shot_quality": {
-        "sq_ranking": 65
+        "sq_ranking": 62
     },
     "sports_reference": {
-        "SRS_rank": "52"
+        "SRS_rank": "50"
     },
     "teamrankings": {
-        "make_tournament": "6.8%",
-        "most_likely_seed": "8",
-        "proj_conf_record": "10.0-10.0",
-        "projected_losses": "11.0",
-        "projected_wins": "20.0",
-        "rank": "55"
+        "make_tournament": "11.9%",
+        "most_likely_seed": "7",
+        "proj_conf_record": "10.7-9.3",
+        "projected_losses": "10.3",
+        "projected_wins": "20.7",
+        "rank": "51"
     },
     "time": {
-        "time": "Jan. 23, 2024 at 10:55 PM"
+        "time": "Jan. 24, 2024 at 10:18 AM"
     },
     "trank": {
-        "torvik_NET": "50",
-        "trank": "43",
-        "trank_make_tourney": "48",
+        "torvik_NET": "47",
+        "trank": "42",
+        "trank_make_tourney": "66.6",
         "trank_proj_record": "21-10 (11-9)",
         "trank_seed": "10",
-        "wab_rank": "32",
+        "wab_rank": "31",
         "wab_score": "1.3"
     },
     "warrennolan": {
