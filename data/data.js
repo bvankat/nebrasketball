e61f4a6fe88b5b6d46affb42a81a8e0eb1710e65
Benjamin Vankat <bvankat@gmail.com>
2024-01-29 09:40:54 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -8,43 +8,43 @@
         "cbs_rank": "8"
     },
     "espn": {
-        "bpi": 57.0,
-        "chance_round_32": "12.0%",
-        "chance_sweet_16": "3.1%",
-        "non_con_sos": 292.0,
-        "proj_tourney_seed": "9",
-        "proj_win_loss_conf": "10.6-9.4",
-        "proj_win_loss_overall": "20.6-10.4",
-        "projected_tournament_order": "33",
-        "seven_day_change": "7th",
-        "sor": 33.0,
-        "sos": 61.0
+        "bpi": 66.0,
+        "chance_round_32": "7.8%",
+        "chance_sweet_16": "1.8%",
+        "non_con_sos": 289.0,
+        "proj_tourney_seed": "10",
+        "proj_win_loss_conf": "10.0-10.0",
+        "proj_win_loss_overall": "20.0-11.0",
+        "projected_tournament_order": "37",
+        "seven_day_change": "Tied-6th",
+        "sor": 38.0,
+        "sos": 47.0
     },
     "espn_bracketology": {
         "seed": "9"
     },
     "evanmiya": {
-        "miya_rating": "63"
+        "miya_rating": "62"
     },
     "haslametrics": {
-        "haslam_rating": "49",
+        "haslam_rating": "48",
         "seed": "11"
     },
     "kenpom": {
         "conference_record": "10-10",
         "overall_record": "20-11",
-        "rating": "53",
+        "rating": "52",
         "record": "15-6",
-        "sos": "61"
+        "sos": "62"
     },
     "kpi_sports": {
         "kpi_ranking": 27
     },
     "massey_ratings": {
-        "massey_rank": "44"
+        "massey_rank": "46"
     },
     "ncaa": {
-        "net_rank": "62"
+        "net_rank": "60"
     },
     "polls": {
         "ap_rank": "NR",
@@ -53,43 +53,43 @@
     "seven_overtimes": {
         "projected_conf_record": "11-9",
         "projected_record": "21-10",
-        "ranking": "49"
+        "ranking": "47"
     },
     "shot_quality": {
-        "sq_ranking": 70
+        "sq_ranking": 68
     },
     "sports_reference": {
-        "SRS_rank": "56"
+        "SRS_rank": "54"
     },
     "teamrankings": {
-        "make_tournament": "9.3%",
+        "make_tournament": "10.3%",
         "most_likely_seed": "8",
         "proj_conf_record": "10.1-9.9",
         "projected_losses": "10.9",
         "projected_wins": "20.1",
-        "rank": "62"
+        "rank": "59"
     },
     "time": {
-        "time": "Jan. 28, 2024 at 10:33 AM"
+        "time": "Jan. 29, 2024 at 9:40 AM"
     },
     "trank": {
-        "torvik_NET": "60",
+        "torvik_NET": "57",
         "trank": "54",
-        "trank_make_tourney": "35.2",
+        "trank_make_tourney": "36.7",
         "trank_proj_record": "20-11 (10-10)",
-        "trank_seed": "11",
+        "trank_seed": "12",
         "wab_rank": "39",
-        "wab_score": "0.9"
+        "wab_score": "1"
     },
     "warrennolan": {
         "current_conf_record": "5-5",
-        "elo": "52",
-        "nolan_net": "62",
+        "elo": "51",
+        "nolan_net": "60",
         "quadrant_1": "3-3",
         "quadrant_2": "2-3",
         "quadrant_3": "3-0",
         "quadrant_4": "7-0",
         "record": "15-6",
-        "rpi": "42"
+        "rpi": "43"
     }
 }
