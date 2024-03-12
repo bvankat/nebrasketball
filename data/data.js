c4dca0877ba2fd82651f7a6a3e04131e91211e2f
Benjamin Vankat <bvankat@gmail.com>
2024-03-12 01:09:36 -0500

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,23 +1,23 @@
 {
     "bracketproject": {
-        "matrix_count": "108",
-        "matrix_seed": "10",
-        "total_brackets": "108"
+        "matrix_count": "114",
+        "matrix_seed": "9",
+        "total_brackets": "114"
     },
     "cbs": {
         "cbs_rank": "10"
     },
     "espn": {
         "bpi": 38.0,
-        "chance_round_32": "53.7%",
-        "chance_sweet_16": "16.5%",
-        "non_con_sos": 273.0,
+        "chance_round_32": "54.2%",
+        "chance_sweet_16": "16.7%",
+        "non_con_sos": 277.0,
         "proj_tourney_seed": "6",
         "proj_win_loss_conf": "12.0-8.0",
         "proj_win_loss_overall": "22.0-9.0",
-        "projected_tournament_order": "23",
+        "projected_tournament_order": "24",
         "seven_day_change": "Tied-1st",
-        "sor": 23.0,
+        "sor": 24.0,
         "sos": 64.0
     },
     "espn_bracketology": {
@@ -30,22 +30,22 @@
     "kenpom": {
         "conference_record": "12-8",
         "overall_record": "22-9",
-        "rating": "32",
+        "rating": "31",
         "record": "22-9",
-        "sos": "71"
+        "sos": "70"
     },
     "kpi_sports": {
         "kpi_ranking": 27
     },
     "massey_ratings": {
-        "massey_rank": "31"
+        "massey_rank": "32"
     },
     "ncaa": {
         "net_rank": "37"
     },
     "polls": {
-        "ap_rank": "NR",
-        "coaches_rank": 38
+        "ap_rank": 35,
+        "coaches_rank": 32
     },
     "shot_quality": {
         "sq_ranking": 43
@@ -62,26 +62,26 @@
         "rank": "36"
     },
     "time": {
-        "time": "Mar. 11, 2024 at 10:07 AM"
+        "time": "Mar. 12, 2024 at 1:09 AM"
     },
     "trank": {
-        "torvik_NET": "37",
+        "torvik_NET": "38",
         "trank": "24",
-        "trank_make_tourney": "99.7",
+        "trank_make_tourney": "99.9",
         "trank_proj_record": "22-9 (12-8)",
-        "trank_seed": "8",
-        "wab_rank": "25",
+        "trank_seed": "9",
+        "wab_rank": "24",
         "wab_score": "+2.2"
     },
     "warrennolan": {
         "current_conf_record": "12-8",
-        "elo": "33",
+        "elo": "34",
         "nolan_net": "37",
         "quadrant_1": "4-6",
         "quadrant_2": "4-3",
         "quadrant_3": "7-0",
         "quadrant_4": "7-0",
         "record": "22-9",
-        "rpi": "37"
+        "rpi": "38"
     }
 }
