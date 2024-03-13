bc37cde592b3b2065376564886ce1c1bc9a680f0
Benjamin Vankat <bvankat@gmail.com>
2024-03-13 15:05:32 -0500

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,30 +1,30 @@
 {
     "bracketproject": {
-        "matrix_count": "123",
+        "matrix_count": "88",
         "matrix_seed": "9",
-        "total_brackets": "123"
+        "total_brackets": "88"
     },
     "cbs": {
         "cbs_rank": "10"
     },
     "espn": {
         "bpi": 38.0,
-        "chance_round_32": "53.7%",
-        "chance_sweet_16": "15.9%",
-        "non_con_sos": 276.0,
+        "chance_round_32": "53.6%",
+        "chance_sweet_16": "16.1%",
+        "non_con_sos": 278.0,
         "proj_tourney_seed": "6",
         "proj_win_loss_conf": "12.0-8.0",
         "proj_win_loss_overall": "22.0-9.0",
-        "projected_tournament_order": "24",
-        "seven_day_change": "Tied-1st",
-        "sor": 24.0,
-        "sos": 64.0
+        "projected_tournament_order": "23",
+        "seven_day_change": "1st",
+        "sor": 23.0,
+        "sos": 65.0
     },
     "espn_bracketology": {
         "seed": "9"
     },
     "haslametrics": {
-        "haslam_rating": "26",
+        "haslam_rating": "27",
         "seed": "8"
     },
     "kenpom": {
@@ -35,7 +35,7 @@
         "sos": "70"
     },
     "kpi_sports": {
-        "kpi_ranking": 25
+        "kpi_ranking": 27
     },
     "massey_ratings": {
         "massey_rank": "31"
@@ -48,7 +48,7 @@
         "coaches_rank": 32
     },
     "shot_quality": {
-        "sq_ranking": 42
+        "sq_ranking": 62
     },
     "sports_reference": {
         "SRS_rank": "36"
@@ -62,10 +62,10 @@
         "rank": "36"
     },
     "time": {
-        "time": "Mar. 13, 2024 at 12:14 AM"
+        "time": "Mar. 13, 2024 at 3:05 PM"
     },
     "trank": {
-        "torvik_NET": "34",
+        "torvik_NET": "37",
         "trank": "24",
         "trank_make_tourney": "99.9",
         "trank_proj_record": "22-9 (12-8)",
@@ -82,6 +82,6 @@
         "quadrant_3": "7-0",
         "quadrant_4": "7-0",
         "record": "22-9",
-        "rpi": "37"
+        "rpi": "38"
     }
 }
