93cf967b97967e543e8ed52f5b42ed0adf95e019
Benjamin Vankat <bvankat@gmail.com>
2024-03-17 09:53:03 -0500

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,90 +1,90 @@
 {
     "bracketproject": {
-        "matrix_count": "120",
-        "matrix_seed": "9",
-        "total_brackets": "120"
+        "matrix_count": "106",
+        "matrix_seed": "8",
+        "total_brackets": "106"
     },
     "cbs": {
         "cbs_rank": "9"
     },
     "espn": {
         "bpi": 37.0,
-        "chance_round_32": "57.8%",
-        "chance_sweet_16": "20.0%",
-        "non_con_sos": 262.0,
+        "chance_round_32": "57.7%",
+        "chance_sweet_16": "19.0%",
+        "non_con_sos": 154.0,
         "proj_tourney_seed": "6",
         "proj_win_loss_conf": "12.0-8.0",
-        "proj_win_loss_overall": "23.3-9.7",
-        "projected_tournament_order": "22",
-        "seven_day_change": "",
-        "sor": 22.0,
-        "sos": 67.0
+        "proj_win_loss_overall": "23.0-10.0",
+        "projected_tournament_order": "24",
+        "seven_day_change": "3rd",
+        "sor": 24.0,
+        "sos": 65.0
     },
     "espn_bracketology": {
-        "seed": "7"
+        "seed": "8"
     },
     "evanmiya": {
-        "miya_rating": "25"
+        "miya_rating": "32"
     },
     "haslametrics": {
-        "haslam_rating": "21",
+        "haslam_rating": "22",
         "seed": "7"
     },
     "kenpom": {
         "conference_record": "12-8",
         "overall_record": "22-9",
-        "rating": "28",
-        "record": "23-9",
-        "sos": "75"
+        "rating": "29",
+        "record": "23-10",
+        "sos": "68"
     },
     "kpi_sports": {
-        "kpi_ranking": 26
+        "kpi_ranking": 27
     },
     "massey_ratings": {
         "massey_rank": "30"
     },
     "ncaa": {
-        "net_rank": "31"
+        "net_rank": "33"
     },
     "polls": {
         "ap_rank": 35,
         "coaches_rank": 32
     },
     "shot_quality": {
-        "sq_ranking": 59
+        "sq_ranking": 57
     },
     "sports_reference": {
-        "SRS_rank": "31"
+        "SRS_rank": "33"
     },
     "teamrankings": {
-        "make_tournament": "99.6%",
+        "make_tournament": "99.3%",
         "most_likely_seed": "7",
         "proj_conf_record": "12.0-8.0",
-        "projected_losses": "9.0",
+        "projected_losses": "10.0",
         "projected_wins": "23.0",
-        "rank": "33"
+        "rank": "34"
     },
     "time": {
-        "time": "Mar. 16, 2024 at 3:57 PM"
+        "time": "Mar. 17, 2024 at 9:53 AM"
     },
     "trank": {
-        "torvik_NET": "30",
+        "torvik_NET": "33",
         "trank": "23",
         "trank_make_tourney": "100",
         "trank_proj_record": "22-9 (12-8)",
-        "trank_seed": "7",
-        "wab_rank": "24",
-        "wab_score": "+2.6"
+        "trank_seed": "8",
+        "wab_rank": "27",
+        "wab_score": "+2.4"
     },
     "warrennolan": {
         "current_conf_record": "12-8",
-        "elo": "27",
-        "nolan_net": "31",
-        "quadrant_1": "4-6",
+        "elo": "31",
+        "nolan_net": "33",
+        "quadrant_1": "4-7",
         "quadrant_2": "5-3",
         "quadrant_3": "7-0",
         "quadrant_4": "7-0",
-        "record": "23-9",
-        "rpi": "32"
+        "record": "23-10",
+        "rpi": "40"
     }
 }
--- data/updates-2024.js
+++ data/updates-2024.js
@@ -23221,6 +23221,96 @@
             "time": {
                 "time": "Mar. 16, 2024 at 3:57 PM"
             }
+        },
+        {
+            "kenpom": {
+                "rating": "29",
+                "record": "23-10",
+                "sos": "68",
+                "overall_record": "22-9",
+                "conference_record": "12-8"
+            },
+            "evanmiya": {
+                "miya_rating": "32"
+            },
+            "ncaa": {
+                "net_rank": "33"
+            },
+            "polls": {
+                "ap_rank": 35,
+                "coaches_rank": 32
+            },
+            "teamrankings": {
+                "make_tournament": "99.3%",
+                "most_likely_seed": "7",
+                "rank": "34",
+                "projected_wins": "23.0",
+                "projected_losses": "10.0",
+                "proj_conf_record": "12.0-8.0"
+            },
+            "espn": {
+                "bpi": 37.0,
+                "seven_day_change": "3rd",
+                "proj_tourney_seed": "6",
+                "proj_win_loss_overall": "23.0-10.0",
+                "proj_win_loss_conf": "12.0-8.0",
+                "sor": 24.0,
+                "sos": 65.0,
+                "non_con_sos": 154.0,
+                "chance_sweet_16": "19.0%",
+                "chance_round_32": "57.7%",
+                "projected_tournament_order": "24"
+            },
+            "espn_bracketology": {
+                "seed": "8"
+            },
+            "warrennolan": {
+                "record": "23-10",
+                "current_conf_record": "12-8",
+                "rpi": "40",
+                "elo": "31",
+                "quadrant_1": "4-7",
+                "quadrant_2": "5-3",
+                "quadrant_3": "7-0",
+                "quadrant_4": "7-0",
+                "nolan_net": "33"
+            },
+            "trank": {
+                "trank": "23",
+                "trank_proj_record": "22-9 (12-8)",
+                "trank_seed": "8",
+                "trank_make_tourney": "100",
+                "wab_score": "+2.4",
+                "wab_rank": "27",
+                "torvik_NET": "33"
+            },
+            "cbs": {
+                "cbs_rank": "9"
+            },
+            "haslametrics": {
+                "haslam_rating": "22",
+                "seed": "7"
+            },
+            "kpi_sports": {
+                "kpi_ranking": 27
+            },
+            "shot_quality": {
+                "sq_ranking": 57
+            },
+            "bracketproject": {
+                "matrix_count": "106",
+                "total_brackets": "106",
+                "matrix_seed": "8"
+            },
+            "massey_ratings": {
+                "massey_rank": "30"
+            },
+            "sports_reference": {
+                "SRS_rank": "33"
+            },
+            "time": {
+                "time": "Mar. 17, 2024 at 9:53 AM"
+            }
         }
     ]
 }
