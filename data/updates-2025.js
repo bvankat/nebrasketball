86e839a03425613b31d8b0b966dfed90b92d4850
Benjamin Vankat <bvankat@gmail.com>
2024-10-29 03:10:34 -0500

    Latest automated update.

--- data/updates-2025.js
+++ data/updates-2025.js
@@ -164,6 +164,74 @@
             "time": {
                 "time": "Oct. 29, 2024 at 3:07 AM"
             }
+        },
+        {
+            "kenpom": {
+                "rating": "57",
+                "record": "0-0",
+                "sos": "1",
+                "overall_record": "16-13",
+                "conference_record": "9-11"
+            },
+            "evanmiya": {
+                "miya_rating": "51"
+            },
+            "polls": {
+                "ap_rank": "NR",
+                "coaches_rank": "NR"
+            },
+            "espn": {
+                "bpi": 37.0,
+                "seven_day_change": "",
+                "proj_tourney_seed": "8",
+                "proj_win_loss_overall": "23.0-11.0",
+                "proj_win_loss_conf": "12.0-8.0",
+                "sor": 31.0,
+                "sos": 59.0,
+                "non_con_sos": 122.0,
+                "chance_sweet_16": "0.0%",
+                "chance_round_32": "0.0%",
+                "projected_tournament_order": "31"
+            },
+            "espn_bracketology": {
+                "seed": "NR"
+            },
+            "warrennolan": {
+                "record": "0-0",
+                "current_conf_record": "0-0",
+                "rpi": "1",
+                "elo": "71",
+                "quadrant_1": "0-0",
+                "quadrant_2": "0-0",
+                "quadrant_3": "0-0",
+                "quadrant_4": "0-0",
+                "nolan_net": "33"
+            },
+            "trank": {
+                "trank": 45,
+                "trank_proj_record": "17-12 (10-10)",
+                "trank_seed": "NR",
+                "trank_make_tourney": "49.4",
+                "wab_score": "NA",
+                "wab_rank": "NA",
+                "torvik_NET": "51"
+            },
+            "cbs": {
+                "cbs_rank": "NR"
+            },
+            "haslametrics": {
+                "haslam_rating": "55",
+                "seed": "NR"
+            },
+            "massey_ratings": {
+                "massey_rank": "46"
+            },
+            "sports_reference": {
+                "SRS_rank": "194"
+            },
+            "time": {
+                "time": "Oct. 29, 2024 at 3:10 AM"
+            }
         }
     ]
 }
