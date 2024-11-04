71d8ddc9312a4cb56fc7d208bad8d9573ab64e5b
Benjamin Vankat <bvankat@gmail.com>
2024-11-04 10:15:29 -0600

    Adds ESPN ratings to graph

--- dot-plot.js
+++ dot-plot.js
@@ -15,8 +15,8 @@ const data = {
 	Kenpom: parseInt(ratings_data.kenpom.rating),
 //    KPI: ratings_data.kpi_sports.kpi_ranking,
     NET: parseInt(ratings_data.trank.torvik_NET), // or use trank.torvik_NET
-//    SOR: parseInt(ratings_data.espn.sor),
-//    BPI: parseInt(ratings_data.espn.bpi),
+    SOR: parseInt(ratings_data.espn.sor),
+    BPI: parseInt(ratings_data.espn.bpi),
     Torvik: parseInt(ratings_data.trank.trank),
     RPI: parseInt(ratings_data.warrennolan.rpi),
     ELO: parseInt(ratings_data.warrennolan.elo),
