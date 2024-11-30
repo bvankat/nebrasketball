fa56acf7dbcdb6c03a78dd79c8383a9cd9d6e93e
Benjamin Vankat <bvankat@gmail.com>
2024-11-30 09:29:45 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -8,42 +8,42 @@
         "cbs_rank": "OUT"
     },
     "espn": {
-        "bpi": 37.0,
-        "chance_round_32": "30.4%",
-        "chance_sweet_16": "10.2%",
-        "non_con_sos": 106.0,
+        "bpi": 40.0,
+        "chance_round_32": "25.8%",
+        "chance_sweet_16": "8.1%",
+        "non_con_sos": 185.0,
         "proj_tourney_seed": 10.0,
-        "proj_win_loss_conf": "10.2-9.8",
-        "proj_win_loss_overall": "17.8-11.2",
-        "projected_tournament_order": 38.0,
-        "seven_day_change": "",
-        "sor": 38.0,
-        "sos": 106.0
+        "proj_win_loss_conf": "9.9-10.1",
+        "proj_win_loss_overall": "17.5-11.5",
+        "projected_tournament_order": 39.0,
+        "seven_day_change": "Tied-4th",
+        "sor": 39.0,
+        "sos": 185.0
     },
     "espn_bracketology": {
         "seed": "11"
     },
     "evanmiya": {
-        "miya_rating": 44
+        "miya_rating": 47
     },
     "haslametrics": {
-        "haslam_rating": 43,
-        "seed": "6"
+        "haslam_rating": 49,
+        "seed": "10"
     },
     "kenpom": {
         "conference_record": "9-11",
         "overall_record": "16-13",
-        "rating": 50,
-        "record": "4-1",
-        "sos": 209
+        "rating": 53,
+        "record": "5-1",
+        "sos": 281
     },
     "massey_ratings": {
-        "massey_rank": 42
+        "massey_rank": 44
     },
     "next_game": {
-        "away_team": "South Dakota",
-        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/233.png",
-        "date_time": "11/27 - 6:30 PM EST",
+        "away_team": "North Florida",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2454.png",
+        "date_time": "12/1 - 4:00 PM EST",
         "home_team": "Nebraska",
         "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
     },
@@ -52,37 +52,37 @@
         "coaches_rank": 36
     },
     "sports_reference": {
-        "SRS_rank": 61
+        "SRS_rank": 59
     },
     "teamrankings": {
-        "make_tournament": "46.2%",
+        "make_tournament": "43.8%",
         "most_likely_seed": "11",
-        "proj_conf_record": "8.4-11.6",
-        "projected_losses": "13.1",
-        "projected_wins": "15.9",
-        "rank": "52"
+        "proj_conf_record": "8.3-11.7",
+        "projected_losses": "13.2",
+        "projected_wins": "15.8",
+        "rank": "55"
     },
     "time": {
-        "time": "Nov. 26, 2024 at 10:22 AM"
+        "time": "Nov. 30, 2024 at 9:29 AM"
     },
     "trank": {
-        "torvik_NET": "46",
-        "trank": 43,
-        "trank_make_tourney": 59.2,
+        "torvik_NET": "44",
+        "trank": 44,
+        "trank_make_tourney": 58.5,
         "trank_proj_record": "17-12 (10-10)",
-        "trank_seed": "10",
-        "wab_rank": 42,
-        "wab_score": "+0.41"
+        "trank_seed": "9",
+        "wab_rank": 43,
+        "wab_score": "+0.43"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "elo": "61",
+        "elo": "58",
         "nolan_net": "33",
         "quadrant_1": "1-1",
         "quadrant_2": "0-0",
         "quadrant_3": "0-0",
-        "quadrant_4": "3-0",
-        "record": "4-1",
-        "rpi": "28"
+        "quadrant_4": "4-0",
+        "record": "5-1",
+        "rpi": "54"
     }
 }
--- data/updates-2025.js
+++ data/updates-2025.js
@@ -4315,6 +4315,94 @@
             "time": {
                 "time": "Nov. 26, 2024 at 10:22 AM"
             }
+        },
+        {
+            "kenpom": {
+                "rating": 53,
+                "record": "5-1",
+                "sos": 281,
+                "overall_record": "16-13",
+                "conference_record": "9-11"
+            },
+            "evanmiya": {
+                "miya_rating": 47
+            },
+            "polls": {
+                "ap_rank": 35,
+                "coaches_rank": 36
+            },
+            "espn": {
+                "bpi": 40.0,
+                "seven_day_change": "Tied-4th",
+                "proj_tourney_seed": 10.0,
+                "proj_win_loss_overall": "17.5-11.5",
+                "proj_win_loss_conf": "9.9-10.1",
+                "sor": 39.0,
+                "sos": 185.0,
+                "non_con_sos": 185.0,
+                "chance_sweet_16": "8.1%",
+                "chance_round_32": "25.8%",
+                "projected_tournament_order": 39.0
+            },
+            "next_game": {
+                "home_team": "Nebraska",
+                "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png",
+                "away_team": "North Florida",
+                "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2454.png",
+                "date_time": "12/1 - 4:00 PM EST"
+            },
+            "espn_bracketology": {
+                "seed": "11"
+            },
+            "teamrankings": {
+                "make_tournament": "43.8%",
+                "most_likely_seed": "11",
+                "rank": "55",
+                "projected_wins": "15.8",
+                "projected_losses": "13.2",
+                "proj_conf_record": "8.3-11.7"
+            },
+            "warrennolan": {
+                "record": "5-1",
+                "current_conf_record": "0-0",
+                "rpi": "54",
+                "elo": "58",
+                "quadrant_1": "1-1",
+                "quadrant_2": "0-0",
+                "quadrant_3": "0-0",
+                "quadrant_4": "4-0",
+                "nolan_net": "33"
+            },
+            "trank": {
+                "trank": 44,
+                "trank_proj_record": "17-12 (10-10)",
+                "trank_seed": "9",
+                "trank_make_tourney": 58.5,
+                "wab_score": "+0.43",
+                "wab_rank": 43,
+                "torvik_NET": "44"
+            },
+            "cbs": {
+                "cbs_rank": "OUT"
+            },
+            "haslametrics": {
+                "haslam_rating": 49,
+                "seed": "10"
+            },
+            "bracketproject": {
+                "matrix_count": 3,
+                "total_brackets": 47,
+                "matrix_seed": "OUT"
+            },
+            "massey_ratings": {
+                "massey_rank": 44
+            },
+            "sports_reference": {
+                "SRS_rank": 59
+            },
+            "time": {
+                "time": "Nov. 30, 2024 at 9:29 AM"
+            }
         }
     ]
 }
