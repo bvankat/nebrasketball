1c2f0360961fd26c049979fb2ba008f39f9bb916
Benjamin Vankat <bvankat@gmail.com>
2024-12-17 23:45:37 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -10,15 +10,15 @@
     "espn": {
         "bpi": 45.0,
         "chance_round_32": "21.3%",
-        "chance_sweet_16": "6.2%",
+        "chance_sweet_16": "6.6%",
         "non_con_sos": 225.0,
         "proj_tourney_seed": 10.0,
         "proj_win_loss_conf": "9.7-10.3",
         "proj_win_loss_overall": "17.3-11.7",
         "projected_tournament_order": 38.0,
-        "seven_day_change": "4th",
+        "seven_day_change": "5th",
         "sor": 38.0,
-        "sos": 89.0
+        "sos": 86.0
     },
     "espn_bracketology": {
         "seed": "First Four Out"
@@ -27,15 +27,15 @@
         "miya_rating": 56
     },
     "haslametrics": {
-        "haslam_rating": 63,
-        "seed": "10"
+        "haslam_rating": 68,
+        "seed": "11"
     },
     "kenpom": {
         "conference_record": "9-11",
         "overall_record": "17-12",
-        "rating": 54,
+        "rating": 53,
         "record": "7-2",
-        "sos": 136
+        "sos": 139
     },
     "kpi_sports": {
         "kpi_ranking": 25
@@ -63,7 +63,7 @@
         "ranking": "43"
     },
     "shot_quality": {
-        "sq_ranking": 40
+        "sq_ranking": 41
     },
     "sports_reference": {
         "SRS_rank": 56
@@ -77,26 +77,26 @@
         "rank": "54"
     },
     "time": {
-        "time": "Dec. 17, 2024 at 9:46 AM"
+        "time": "Dec. 17, 2024 at 11:45 PM"
     },
     "trank": {
         "torvik_NET": "57",
         "trank": 46,
-        "trank_make_tourney": 45.6,
+        "trank_make_tourney": 46.1,
         "trank_proj_record": "17-12 (10-10)",
         "trank_seed": "11",
-        "wab_rank": 40,
+        "wab_rank": 42,
         "wab_score": "+0.6"
     },
     "warrennolan": {
         "current_conf_record": "1-1",
-        "elo": "51",
+        "elo": "53",
         "nolan_net": "57",
         "quadrant_1": "1-2",
         "quadrant_2": "1-0",
         "quadrant_3": "1-0",
         "quadrant_4": "4-0",
         "record": "7-2",
-        "rpi": "28"
+        "rpi": "29"
     }
 }
