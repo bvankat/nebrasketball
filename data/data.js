05e1e44712eb1fb549a16c23e1e990b9885a6cca
Benjamin Vankat <bvankat@gmail.com>
2024-12-24 12:04:18 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -24,10 +24,10 @@
         "seed": "First Four Out"
     },
     "evanmiya": {
-        "miya_rating": 50
+        "miya_rating": 48
     },
     "haslametrics": {
-        "haslam_rating": 72,
+        "haslam_rating": 65,
         "seed": "OUT"
     },
     "kenpom": {
@@ -35,23 +35,23 @@
         "overall_record": "19-12",
         "rating": 45,
         "record": "9-2",
-        "sos": 117
+        "sos": 121
     },
     "kpi_sports": {
-        "kpi_ranking": 24
+        "kpi_ranking": 20
     },
     "massey_ratings": {
-        "massey_rank": 46
+        "massey_rank": 47
     },
     "ncaa": {
-        "net_rank": "57"
+        "net_rank": "52"
     },
     "next_game": {
-        "away_team": "Nebraska",
-        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png",
-        "date_time": "Final",
-        "home_team": "Hawai'i",
-        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/62.png"
+        "away_team": "Southern",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2582.png",
+        "date_time": "12/30 - 9:00 PM EST",
+        "home_team": "Nebraska",
+        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
     },
     "polls": {
         "ap_rank": "NR",
@@ -60,43 +60,43 @@
     "seven_overtimes": {
         "projected_conf_record": "9-7",
         "projected_record": "20-10",
-        "ranking": "34"
+        "ranking": "35"
     },
     "shot_quality": {
-        "sq_ranking": 56
+        "sq_ranking": 54
     },
     "sports_reference": {
         "SRS_rank": 54
     },
     "teamrankings": {
-        "make_tournament": "52.5%",
+        "make_tournament": "60.1%",
         "most_likely_seed": "11",
-        "proj_conf_record": "8.6-11.4",
+        "proj_conf_record": "8.8-11.2",
         "projected_losses": "12.6",
-        "projected_wins": "17.4",
+        "projected_wins": "18.4",
         "rank": "49"
     },
     "time": {
-        "time": "Dec. 24, 2024 at 12:44 AM"
+        "time": "Dec. 24, 2024 at 12:04 PM"
     },
     "trank": {
-        "torvik_NET": "49",
+        "torvik_NET": "48",
         "trank": 39,
-        "trank_make_tourney": 52.9,
+        "trank_make_tourney": 59.2,
         "trank_proj_record": "19-12 (10-10)",
-        "trank_seed": "10",
+        "trank_seed": "11",
         "wab_rank": 34,
         "wab_score": "+0.9"
     },
     "warrennolan": {
         "current_conf_record": "1-1",
         "elo": "43",
-        "nolan_net": "57",
+        "nolan_net": "52",
         "quadrant_1": "1-1",
         "quadrant_2": "1-1",
         "quadrant_3": "2-0",
         "quadrant_4": "5-0",
         "record": "9-2",
-        "rpi": "24"
+        "rpi": "25"
     }
 }
