9f575a6d8f16c6440bd397cf203a2551624326f1
Benjamin Vankat <bvankat@gmail.com>
2024-12-31 11:47:47 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -21,10 +21,10 @@
         "sos": 64.0
     },
     "espn_bracketology": {
-        "seed": "First Four Out"
+        "seed": "11"
     },
     "evanmiya": {
-        "miya_rating": 50
+        "miya_rating": 44
     },
     "haslametrics": {
         "haslam_rating": 51,
@@ -38,18 +38,18 @@
         "sos": 124
     },
     "kpi_sports": {
-        "kpi_ranking": 15
+        "kpi_ranking": 16
     },
     "massey_ratings": {
         "massey_rank": 34
     },
     "ncaa": {
-        "net_rank": "46"
+        "net_rank": "38"
     },
     "next_game": {
-        "away_team": "Southern",
-        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2582.png",
-        "date_time": "Final",
+        "away_team": "UCLA",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/26.png",
+        "date_time": "1/4 - 2:00 PM EST",
         "home_team": "Nebraska",
         "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
     },
@@ -60,38 +60,38 @@
     "seven_overtimes": {
         "projected_conf_record": "9-7",
         "projected_record": "21-10",
-        "ranking": "23"
+        "ranking": "28"
     },
     "shot_quality": {
-        "sq_ranking": 50
+        "sq_ranking": 56
     },
     "sports_reference": {
-        "SRS_rank": 49
+        "SRS_rank": 47
     },
     "teamrankings": {
-        "make_tournament": "66.0%",
+        "make_tournament": "72.6%",
         "most_likely_seed": "11",
-        "proj_conf_record": "8.9-11.1",
-        "projected_losses": "12.1",
-        "projected_wins": "18.9",
+        "proj_conf_record": "9.4-10.6",
+        "projected_losses": "11.6",
+        "projected_wins": "19.4",
         "rank": "40"
     },
     "time": {
-        "time": "Dec. 31, 2024 at 1:03 AM"
+        "time": "Dec. 31, 2024 at 11:47 AM"
     },
     "trank": {
-        "torvik_NET": "38",
+        "torvik_NET": "34",
         "trank": 28,
-        "trank_make_tourney": 73.4,
+        "trank_make_tourney": 84.7,
         "trank_proj_record": "20-11 (10-10)",
-        "trank_seed": "9",
+        "trank_seed": "8",
         "wab_rank": 24,
         "wab_score": "+1.3"
     },
     "warrennolan": {
         "current_conf_record": "1-1",
         "elo": "35",
-        "nolan_net": "46",
+        "nolan_net": "38",
         "quadrant_1": "1-1",
         "quadrant_2": "2-1",
         "quadrant_3": "2-0",
