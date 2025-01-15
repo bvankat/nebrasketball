d828450c07dbba6c6524a1f172ff9ec31340facd
Benjamin Vankat <bvankat@gmail.com>
2025-01-15 01:00:47 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,24 +1,24 @@
 {
     "bracketproject": {
-        "matrix_count": 49,
-        "matrix_seed": 8,
-        "total_brackets": 51
+        "matrix_count": 58,
+        "matrix_seed": 10,
+        "total_brackets": 59
     },
     "cbs": {
         "cbs_rank": "9"
     },
     "espn": {
         "bpi": 40.0,
-        "chance_round_32": "25.0%",
-        "chance_sweet_16": "7.1%",
-        "non_con_sos": 153.0,
+        "chance_round_32": "25.6%",
+        "chance_sweet_16": "7.6%",
+        "non_con_sos": 155.0,
         "proj_tourney_seed": 9.0,
         "proj_win_loss_conf": "9.5-10.5",
         "proj_win_loss_overall": "19.5-11.5",
-        "projected_tournament_order": 33.0,
+        "projected_tournament_order": 35.0,
         "seven_day_change": "Tied-3rd",
-        "sor": 33.0,
-        "sos": 48.0
+        "sor": 35.0,
+        "sos": 47.0
     },
     "espn_bracketology": {
         "seed": "10"
@@ -27,15 +27,15 @@
         "miya_rating": 53
     },
     "haslametrics": {
-        "haslam_rating": 50,
+        "haslam_rating": 49,
         "seed": "10"
     },
     "kenpom": {
-        "conference_record": "10-10",
-        "overall_record": "20-11",
+        "conference_record": "9-11",
+        "overall_record": "19-12",
         "rating": 41,
         "record": "12-4",
-        "sos": 37
+        "sos": 41
     },
     "kpi_sports": {
         "kpi_ranking": 27
@@ -71,32 +71,32 @@
     "teamrankings": {
         "make_tournament": "70.4%",
         "most_likely_seed": "11",
-        "proj_conf_record": "8.8-11.2",
-        "projected_losses": "12.2",
-        "projected_wins": "18.8",
-        "rank": "49"
+        "proj_conf_record": "8.9-11.1",
+        "projected_losses": "12.1",
+        "projected_wins": "18.9",
+        "rank": "50"
     },
     "time": {
-        "time": "Jan. 14, 2025 at 2:16 PM"
+        "time": "Jan. 15, 2025 at 1:00 AM"
     },
     "trank": {
-        "torvik_NET": "41",
-        "trank": 35,
+        "torvik_NET": "43",
+        "trank": 38,
         "trank_make_tourney": 79.4,
         "trank_proj_record": "20-11 (10-10)",
-        "trank_seed": "9",
+        "trank_seed": "10",
         "wab_rank": 33,
         "wab_score": "+1.5"
     },
     "warrennolan": {
         "current_conf_record": "2-3",
-        "elo": "42",
+        "elo": "41",
         "nolan_net": "45",
         "quadrant_1": "1-4",
         "quadrant_2": "3-0",
         "quadrant_3": "2-0",
         "quadrant_4": "6-0",
         "record": "12-4",
-        "rpi": "23"
+        "rpi": "21"
     }
 }
--- data/updates-2025.js
+++ data/updates-2025.js
@@ -10041,6 +10041,108 @@
             "time": {
                 "time": "Jan. 14, 2025 at 2:16 PM"
             }
+        },
+        {
+            "kenpom": {
+                "rating": 41,
+                "record": "12-4",
+                "sos": 41,
+                "overall_record": "19-12",
+                "conference_record": "9-11"
+            },
+            "evanmiya": {
+                "miya_rating": 53
+            },
+            "polls": {
+                "ap_rank": "NR",
+                "coaches_rank": "NR"
+            },
+            "espn": {
+                "bpi": 40.0,
+                "seven_day_change": "Tied-3rd",
+                "proj_tourney_seed": 9.0,
+                "proj_win_loss_overall": "19.5-11.5",
+                "proj_win_loss_conf": "9.5-10.5",
+                "sor": 35.0,
+                "sos": 47.0,
+                "non_con_sos": 155.0,
+                "chance_sweet_16": "7.6%",
+                "chance_round_32": "25.6%",
+                "projected_tournament_order": 35.0
+            },
+            "next_game": {
+                "home_team": "Nebraska",
+                "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png",
+                "away_team": "Rutgers",
+                "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/164.png",
+                "date_time": "1/16 - 9:00 PM EST"
+            },
+            "espn_bracketology": {
+                "seed": "10"
+            },
+            "ncaa": {
+                "net_rank": "45"
+            },
+            "teamrankings": {
+                "make_tournament": "70.4%",
+                "most_likely_seed": "11",
+                "rank": "50",
+                "projected_wins": "18.9",
+                "projected_losses": "12.1",
+                "proj_conf_record": "8.9-11.1"
+            },
+            "warrennolan": {
+                "record": "12-4",
+                "current_conf_record": "2-3",
+                "rpi": "21",
+                "elo": "41",
+                "quadrant_1": "1-4",
+                "quadrant_2": "3-0",
+                "quadrant_3": "2-0",
+                "quadrant_4": "6-0",
+                "nolan_net": "45"
+            },
+            "trank": {
+                "trank": 38,
+                "trank_proj_record": "20-11 (10-10)",
+                "trank_seed": "10",
+                "trank_make_tourney": 79.4,
+                "wab_score": "+1.5",
+                "wab_rank": 33,
+                "torvik_NET": "43"
+            },
+            "cbs": {
+                "cbs_rank": "9"
+            },
+            "haslametrics": {
+                "haslam_rating": 49,
+                "seed": "10"
+            },
+            "kpi_sports": {
+                "kpi_ranking": 27
+            },
+            "shot_quality": {
+                "sq_ranking": 52
+            },
+            "bracketproject": {
+                "matrix_count": 58,
+                "total_brackets": 59,
+                "matrix_seed": 10
+            },
+            "massey_ratings": {
+                "massey_rank": 35
+            },
+            "seven_overtimes": {
+                "ranking": "38",
+                "projected_record": "21-10",
+                "projected_conf_record": "11-9"
+            },
+            "sports_reference": {
+                "SRS_rank": 49
+            },
+            "time": {
+                "time": "Jan. 15, 2025 at 1:00 AM"
+            }
         }
     ]
 }
