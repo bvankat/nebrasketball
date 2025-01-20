31b727814f479719b3248092cbc7448acb223368
Benjamin Vankat <bvankat@gmail.com>
2025-01-20 17:56:11 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,75 +1,75 @@
 {
     "bracketproject": {
-        "matrix_count": 58,
+        "matrix_count": 65,
         "matrix_seed": 10,
-        "total_brackets": 59
+        "total_brackets": 68
     },
     "cbs": {
         "cbs_rank": "11"
     },
     "espn": {
-        "bpi": 46.0,
-        "chance_round_32": "20.6%",
-        "chance_sweet_16": "5.1%",
-        "non_con_sos": 158.0,
+        "bpi": 45.0,
+        "chance_round_32": "19.9%",
+        "chance_sweet_16": "5.2%",
+        "non_con_sos": 165.0,
         "proj_tourney_seed": 12.0,
-        "proj_win_loss_conf": "8.4-11.6",
-        "proj_win_loss_overall": "18.4-12.6",
-        "projected_tournament_order": 71.0,
-        "seven_day_change": "Tied-9th",
-        "sor": 48.0,
-        "sos": 49.0
+        "proj_win_loss_conf": "8.3-11.7",
+        "proj_win_loss_overall": "18.3-12.7",
+        "projected_tournament_order": 74.0,
+        "seven_day_change": "Tied-5th",
+        "sor": 50.0,
+        "sos": 43.0
     },
     "espn_bracketology": {
         "seed": "10"
     },
     "evanmiya": {
-        "miya_rating": 54
+        "miya_rating": 48
     },
     "haslametrics": {
-        "haslam_rating": 57,
+        "haslam_rating": 53,
         "seed": "PI11"
     },
     "kenpom": {
         "conference_record": "8-12",
         "overall_record": "18-13",
-        "rating": 45,
-        "record": "12-5",
-        "sos": 43
+        "rating": 46,
+        "record": "12-6",
+        "sos": 38
     },
     "kpi_sports": {
-        "kpi_ranking": 33
+        "kpi_ranking": 37
     },
     "massey_ratings": {
-        "massey_rank": 35
+        "massey_rank": 46
     },
     "ncaa": {
-        "net_rank": "50"
+        "net_rank": "49"
     },
     "next_game": {
-        "away_team": "Nebraska",
-        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png",
-        "date_time": "1/19 - 12:00 PM EST",
-        "home_team": "Maryland",
-        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/120.png"
+        "away_team": "USC",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/30.png",
+        "date_time": "1/22 - 9:00 PM EST",
+        "home_team": "Nebraska",
+        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
     },
     "polls": {
         "ap_rank": "NR",
         "coaches_rank": "NR"
     },
     "seven_overtimes": {
-        "projected_conf_record": "10-10",
-        "projected_record": "20-11",
-        "ranking": "35"
+        "projected_conf_record": "9-11",
+        "projected_record": "19-12",
+        "ranking": "38"
     },
     "shot_quality": {
-        "sq_ranking": 48
+        "sq_ranking": 50
     },
     "sports_reference": {
         "SRS_rank": 53
     },
     "teamrankings": {
-        "make_tournament": "53.3%",
+        "make_tournament": "49.6%",
         "most_likely_seed": "11",
         "proj_conf_record": "7.8-12.2",
         "projected_losses": "13.2",
@@ -77,26 +77,26 @@
         "rank": "51"
     },
     "time": {
-        "time": "Jan. 17, 2025 at 1:21 PM"
+        "time": "Jan. 20, 2025 at 5:56 PM"
     },
     "trank": {
-        "torvik_NET": "52",
-        "trank": 41,
-        "trank_make_tourney": 50.4,
+        "torvik_NET": "51",
+        "trank": 39,
+        "trank_make_tourney": 47.3,
         "trank_proj_record": "19-12 (9-11)",
         "trank_seed": "11",
-        "wab_rank": 44,
-        "wab_score": "+0.7"
+        "wab_rank": 46,
+        "wab_score": "+0.41"
     },
     "warrennolan": {
-        "current_conf_record": "2-4",
-        "elo": "53",
-        "nolan_net": "50",
-        "quadrant_1": "1-4",
+        "current_conf_record": "2-5",
+        "elo": "68",
+        "nolan_net": "49",
+        "quadrant_1": "1-5",
         "quadrant_2": "3-0",
         "quadrant_3": "2-1",
         "quadrant_4": "6-0",
-        "record": "12-5",
-        "rpi": "35"
+        "record": "12-6",
+        "rpi": "34"
     }
 }
