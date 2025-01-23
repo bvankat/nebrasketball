1329e27ed161469d636440309ae099bcb70c15e7
Benjamin Vankat <bvankat@gmail.com>
2025-01-22 22:15:19 -0600

    Post USC updates

--- js/load-data.js
+++ js/load-data.js
@@ -194,7 +194,7 @@
 	 
 	 // Intangibles: Don't think any of the systems has a way to value injuries and/or hype, so we need to add it. Check out sports-reference Win Shares, perhaps, for injuries data points. 
 	 
-	 intangibles = -5;
+	 intangibles = -20;
 	 intangibles_msg = "Doesn't pass eye test against good teams.";
 	 
 	 // Penalty for a Nonconference SOS above 250. New for 2025.
