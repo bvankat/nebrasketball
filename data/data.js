b4fc49b4860f09ad135dc7d03de570148cd373c5
Benjamin Vankat <bvankat@gmail.com>
2025-01-24 09:23:33 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,8 +1,8 @@
 {
     "bracketproject": {
-        "matrix_count": 58,
+        "matrix_count": 48,
         "matrix_seed": 11,
-        "total_brackets": 74
+        "total_brackets": 77
     },
     "cbs": {
         "cbs_rank": "11"
@@ -21,13 +21,13 @@
         "sos": 43.0
     },
     "espn_bracketology": {
-        "seed": "11"
+        "seed": "First Four Out"
     },
     "evanmiya": {
-        "miya_rating": 53
+        "miya_rating": 51
     },
     "haslametrics": {
-        "haslam_rating": 56,
+        "haslam_rating": 57,
         "seed": "OUT"
     },
     "kenpom": {
@@ -35,23 +35,23 @@
         "overall_record": "17-14",
         "rating": 50,
         "record": "12-7",
-        "sos": 37
+        "sos": 36
     },
     "kpi_sports": {
-        "kpi_ranking": 43
+        "kpi_ranking": 45
     },
     "massey_ratings": {
         "massey_rank": 46
     },
     "ncaa": {
-        "net_rank": "57"
+        "net_rank": "55"
     },
     "next_game": {
-        "away_team": "USC",
-        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/30.png",
-        "date_time": "Final",
-        "home_team": "Nebraska",
-        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
+        "away_team": "Nebraska",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png",
+        "date_time": "1/26 - 1:00 PM EST",
+        "home_team": "Wisconsin",
+        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/275.png"
     },
     "polls": {
         "ap_rank": "NR",
@@ -60,43 +60,43 @@
     "seven_overtimes": {
         "projected_conf_record": "8-12",
         "projected_record": "18-13",
-        "ranking": "35"
+        "ranking": "36"
     },
     "shot_quality": {
         "sq_ranking": 56
     },
     "sports_reference": {
-        "SRS_rank": 54
+        "SRS_rank": 55
     },
     "teamrankings": {
-        "make_tournament": "35.1%",
+        "make_tournament": "35.9%",
         "most_likely_seed": "11",
         "proj_conf_record": "6.9-13.1",
         "projected_losses": "14.1",
         "projected_wins": "16.9",
-        "rank": "53"
+        "rank": "52"
     },
     "time": {
-        "time": "Jan. 23, 2025 at 10:04 AM"
+        "time": "Jan. 24, 2025 at 9:23 AM"
     },
     "trank": {
-        "torvik_NET": "63",
-        "trank": 40,
-        "trank_make_tourney": 32.2,
+        "torvik_NET": "59",
+        "trank": 42,
+        "trank_make_tourney": 30.5,
         "trank_proj_record": "18-13 (8-12)",
         "trank_seed": "OUT",
-        "wab_rank": 56,
-        "wab_score": "-0.15"
+        "wab_rank": 54,
+        "wab_score": "-0.13"
     },
     "warrennolan": {
         "current_conf_record": "2-6",
-        "elo": "78",
-        "nolan_net": "57",
+        "elo": "80",
+        "nolan_net": "55",
         "quadrant_1": "1-5",
         "quadrant_2": "3-1",
         "quadrant_3": "2-1",
         "quadrant_4": "6-0",
         "record": "12-7",
-        "rpi": "43"
+        "rpi": "44"
     }
 }
