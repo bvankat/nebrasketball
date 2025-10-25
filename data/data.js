ef6bbb6f9499db2fb2b95a74e50bafb9bbeadef8
Benjamin Vankat <bvankat@gmail.com>
2025-10-25 17:15:19 -0500

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -1,11 +1,57 @@
 {
-    "time": {
-        "time": "Oct. 25, 2025 at 5:02 PM"
+    "espn": {
+        "bpi": 47.0,
+        "chance_round_32": "",
+        "chance_sweet_16": "",
+        "non_con_sos": 81.0,
+        "proj_tourney_seed": 13.0,
+        "proj_win_loss_conf": "7.0-13.0",
+        "proj_win_loss_overall": "21.0-14.0",
+        "projected_tournament_order": 71.0,
+        "seven_day_change": "",
+        "sor": 51.0,
+        "sos": 40.0
     },
-    "trank": {
-        "torvik_NET": "56",
-        "trank": 44,
-        "trank_proj_record": "18-12 (9-11)",
-        "trank_seed": "NA"
+    "espn_bracketology": {
+        "seed": "OUT"
+    },
+    "haslametrics": {
+        "haslam_rating": "40",
+        "seed": "PI11"
+    },
+    "kenpom": {
+        "conference_record": "8-12",
+        "overall_record": "17-13",
+        "rating": 51,
+        "record": "0-0",
+        "sos": 1
+    },
+    "kpi_sports": {
+        "kpi_ranking": null
+    },
+    "next_game": {
+        "away_team": "West Georgia",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2698.png",
+        "date_time": "11/3 - 8:00 PM EST",
+        "home_team": "Nebraska",
+        "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
+    },
+    "polls": {
+        "ap_rank": "NR",
+        "coaches_rank": "NR"
+    },
+    "time": {
+        "time": "Oct. 25, 2025 at 5:14 PM"
+    },
+    "warrennolan": {
+        "current_conf_record": "0-0",
+        "elo": "69",
+        "nolan_net": "54",
+        "quadrant_1": "0-0",
+        "quadrant_2": "0-0",
+        "quadrant_3": "0-0",
+        "quadrant_4": "0-0",
+        "record": "0-0",
+        "rpi": "1"
     }
 }
