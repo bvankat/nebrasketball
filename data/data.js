d8ee55614dfb149477143db359f5bdca6ead18f9
Benjamin Vankat <bvankat@gmail.com>
2025-11-10 09:57:15 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -15,9 +15,18 @@
                 "opponent_rank": null,
                 "result": "W",
                 "score": "86-53"
+            },
+            {
+                "date": "11/08/2025",
+                "location": "Home",
+                "opponent": "FIU",
+                "opponent_NET": "260",
+                "opponent_rank": null,
+                "result": "W",
+                "score": "96-66"
             }
         ],
-        "quad4record": "1-0"
+        "quad4record": "2-0"
     },
     "bracketproject": {
         "matrix_count": 10,
@@ -28,14 +37,14 @@
         "bpi": 32.0,
         "chance_round_32": "",
         "chance_sweet_16": "",
-        "non_con_sos": 316.0,
+        "non_con_sos": 326.0,
         "proj_tourney_seed": "OUT",
-        "proj_win_loss_conf": "11.2-8.8",
-        "proj_win_loss_overall": "20.1-9.9",
+        "proj_win_loss_conf": "11.1-8.9",
+        "proj_win_loss_overall": "20.0-10.0",
         "projected_tournament_order": "",
         "seven_day_change": "",
-        "sor": 110.0,
-        "sos": 316.0
+        "sor": 114.0,
+        "sos": 326.0
     },
     "espn_bracketology": {
         "seed": "OUT"
@@ -43,18 +52,18 @@
     "evanmiya": {
         "miya_rating": 44,
         "miya_resume_category": "Just In The Field",
-        "miya_resume_rank": 87
+        "miya_resume_rank": 88
     },
     "haslametrics": {
         "haslam_rating": "34",
-        "seed": "9"
+        "seed": "10"
     },
     "kenpom": {
         "conference_record": "9-11",
         "overall_record": "18-12",
-        "rating": 43,
+        "rating": 42,
         "record": "2-0",
-        "sos": 293
+        "sos": 302
     },
     "next_game": {
         "away_team": "MD Eastern",
@@ -71,35 +80,35 @@
         "SRS_rank": "NA"
     },
     "teamrankings": {
-        "make_tournament": "48.8%",
-        "most_likely_seed": "8",
+        "make_tournament": "48.7%",
+        "most_likely_seed": "9",
         "proj_conf_record": "9.5-10.5",
-        "projected_bid_pct": "48.8%",
-        "projected_losses": "11.7",
-        "projected_wins": "18.3",
+        "projected_bid_pct": "48.7%",
+        "projected_losses": "11.8",
+        "projected_wins": "18.2",
         "rank": "39"
     },
     "time": {
-        "time": "Nov. 9 at 11:31 AM"
+        "time": "Nov. 10 at 9:54 AM"
     },
     "trank": {
-        "torvik_NET": "50",
-        "trank": 42,
-        "trank_make_tourney": 53.6,
+        "torvik_NET": "51",
+        "trank": 41,
+        "trank_make_tourney": 54.2,
         "trank_proj_record": "18-12 (9-11)",
         "trank_seed": "11",
-        "wab_rank": 78,
+        "wab_rank": 82,
         "wab_score": "+0.08"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
-        "elo": "64",
+        "elo": "62",
         "nolan_net": "54",
         "quadrant_1": "0-0",
         "quadrant_2": "0-0",
         "quadrant_3": "0-0",
         "quadrant_4": "2-0",
         "record": "2-0",
-        "rpi": "73"
+        "rpi": "105"
     }
 }
