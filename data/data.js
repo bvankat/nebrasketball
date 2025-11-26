73d63f5525b799a88a5e62dcd022b08cb21a2529
Benjamin Vankat <bvankat@gmail.com>
2025-11-26 12:28:47 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -62,9 +62,18 @@
                 "opponent_rank": null,
                 "result": "W",
                 "score": "69-50"
+            },
+            {
+                "date": "11/25/2025",
+                "location": "Home",
+                "opponent": "Winthrop",
+                "opponent_NET": "163",
+                "opponent_rank": null,
+                "result": "W",
+                "score": "80-73"
             }
         ],
-        "quad4record": "3-0"
+        "quad4record": "4-0"
     },
     "bracketproject": {
         "matrix_count": 10,
@@ -75,41 +84,41 @@
         "bpi": 35.0,
         "chance_round_32": "",
         "chance_sweet_16": "",
-        "non_con_sos": 109.0,
+        "non_con_sos": 136.0,
         "proj_tourney_seed": "OUT",
-        "proj_win_loss_conf": "10.4-9.6",
-        "proj_win_loss_overall": "21.0-10.0",
+        "proj_win_loss_conf": "10.1-9.9",
+        "proj_win_loss_overall": "20.7-10.3",
         "projected_tournament_order": "",
-        "seven_day_change": "2nd",
-        "sor": 7.0,
-        "sos": 109.0
+        "seven_day_change": "3rd",
+        "sor": 9.0,
+        "sos": 136.0
     },
     "espn_bracketology": {
         "seed": "10"
     },
     "evanmiya": {
-        "miya_rating": 43,
+        "miya_rating": 41,
         "miya_resume_category": "3-5 Seed",
-        "miya_resume_rank": 10
+        "miya_resume_rank": 9
     },
     "haslametrics": {
-        "haslam_rating": "23",
-        "seed": "3"
+        "haslam_rating": "25",
+        "seed": "5"
     },
     "kenpom": {
         "conference_record": "10-10",
         "overall_record": "20-11",
-        "rating": 40,
-        "record": "6-0",
-        "sos": 199
+        "rating": 41,
+        "record": "7-0",
+        "sos": 182
     },
     "massey_ratings": {
-        "massey_rank": 30
+        "massey_rank": 27
     },
     "next_game": {
-        "away_team": "Winthrop",
-        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2737.png",
-        "date_time": "11/25 - 8:30 PM EST",
+        "away_team": "SC Upstate",
+        "away_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/2908.png",
+        "date_time": "11/29 - 2:00 PM EST",
         "home_team": "Nebraska",
         "home_team_logo": "https://a.espncdn.com/i/teamlogos/ncaa/500/158.png"
     },
@@ -121,26 +130,26 @@
         "SRS_rank": "NA"
     },
     "teamrankings": {
-        "make_tournament": "65.3%",
-        "most_likely_seed": "10",
-        "proj_conf_record": "9.6-10.4",
-        "projected_bid_pct": "65.3%",
-        "projected_losses": "10.9",
-        "projected_wins": "20.1",
-        "rank": "42"
+        "make_tournament": "64.6%",
+        "most_likely_seed": "11",
+        "proj_conf_record": "9.4-10.6",
+        "projected_bid_pct": "64.6%",
+        "projected_losses": "11.0",
+        "projected_wins": "20.0",
+        "rank": "43"
     },
     "time": {
-        "iso_time": "2025-11-25T09:12:33.734805",
-        "time": "Nov. 25 at 9:12 AM"
+        "iso_time": "2025-11-26T12:28:41.839237",
+        "time": "Nov. 26 at 12:28 PM"
     },
     "trank": {
-        "torvik_NET": "37",
-        "trank": 35,
-        "trank_make_tourney": 84.1,
+        "torvik_NET": "34",
+        "trank": 34,
+        "trank_make_tourney": 84.3,
         "trank_proj_record": "20-11 (10-10)",
         "trank_seed": "8",
-        "wab_rank": 8,
-        "wab_score": "+1.3"
+        "wab_rank": 6,
+        "wab_score": "+1.5"
     },
     "warrennolan": {
         "current_conf_record": "0-0",
@@ -149,8 +158,8 @@
         "quadrant_1": "2-0",
         "quadrant_2": "1-0",
         "quadrant_3": "0-0",
-        "quadrant_4": "3-0",
-        "record": "6-0",
-        "rpi": "9"
+        "quadrant_4": "4-0",
+        "record": "7-0",
+        "rpi": "10"
     }
 }
