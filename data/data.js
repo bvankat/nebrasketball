9b8b99b9e6700757f35f03e4d2f1d9a22f47acba
Benjamin Vankat <bvankat@gmail.com>
2025-12-20 13:54:30 -0600

    Latest automated update.

--- data/data.js
+++ data/data.js
@@ -17,7 +17,7 @@
                 "date": "11/15/2025",
                 "location": "Neutral",
                 "opponent": "Oklahoma",
-                "opponent_NET": "56",
+                "opponent_NET": "58",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "105-99"
@@ -26,7 +26,7 @@
                 "date": "11/20/2025",
                 "location": "Neutral",
                 "opponent": "New Mexico",
-                "opponent_NET": "66",
+                "opponent_NET": "65",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "84-72"
@@ -35,19 +35,19 @@
                 "date": "11/21/2025",
                 "location": "Neutral",
                 "opponent": "Kansas St.",
-                "opponent_NET": "61",
+                "opponent_NET": "60",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "86-85"
             },
             {
-                "date": "12/10/2025",
+                "date": "12/07/2025",
                 "location": "Home",
-                "opponent": "Wisconsin",
-                "opponent_NET": "68",
+                "opponent": "Creighton",
+                "opponent_NET": "71",
                 "opponent_rank": null,
                 "result": "W",
-                "score": "90-60"
+                "score": "71-50"
             }
         ],
         "quad2record": "4-0",
@@ -56,19 +56,19 @@
                 "date": "11/25/2025",
                 "location": "Home",
                 "opponent": "Winthrop",
-                "opponent_NET": "102",
+                "opponent_NET": "124",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "80-73"
             },
             {
-                "date": "12/07/2025",
+                "date": "12/10/2025",
                 "location": "Home",
-                "opponent": "Creighton",
-                "opponent_NET": "79",
+                "opponent": "Wisconsin",
+                "opponent_NET": "77",
                 "opponent_rank": null,
                 "result": "W",
-                "score": "71-50"
+                "score": "90-60"
             }
         ],
         "quad3record": "2-0",
@@ -77,7 +77,7 @@
                 "date": "11/03/2025",
                 "location": "Home",
                 "opponent": "West Ga.",
-                "opponent_NET": "266",
+                "opponent_NET": "261",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "86-53"
@@ -86,7 +86,7 @@
                 "date": "11/08/2025",
                 "location": "Home",
                 "opponent": "FIU",
-                "opponent_NET": "178",
+                "opponent_NET": "183",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "96-66"
@@ -95,7 +95,7 @@
                 "date": "11/11/2025",
                 "location": "Home",
                 "opponent": "UMES",
-                "opponent_NET": "334",
+                "opponent_NET": "331",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "69-50"
@@ -104,7 +104,7 @@
                 "date": "11/29/2025",
                 "location": "Home",
                 "opponent": "USC Upstate",
-                "opponent_NET": "198",
+                "opponent_NET": "202",
                 "opponent_rank": null,
                 "result": "W",
                 "score": "72-63"
@@ -113,44 +113,32 @@
         "quad4record": "4-0"
     },
     "bracketproject": {
-        "matrix_count": 33,
-        "matrix_seed": 4,
-        "total_brackets": 33
+        "matrix_count": 35,
+        "matrix_seed": 3,
+        "total_brackets": 35
     },
     "espn": {
         "bpi": 26.0,
         "chance_round_32": "NA",
         "chance_sweet_16": "NA",
-        "non_con_sos": 129.0,
+        "non_con_sos": 130.0,
         "proj_tourney_seed": "NA",
         "proj_win_loss_conf": "12.3-7.7",
-        "proj_win_loss_overall": "23.2-7.8",
+        "proj_win_loss_overall": "23.3-7.7",
         "projected_tournament_order": "NA",
         "seven_day_change": "1st",
         "sor": 6.0,
-        "sos": 41.0
+        "sos": 42.0
     },
     "espn_bracketology": {
         "seed": "5"
     },
-    "evanmiya": {
-        "miya_rating": 25,
-        "miya_resume_category": "1-2 Seed",
-        "miya_resume_rank": 7
-    },
     "haslametrics": {
-        "haslam_rating": "12",
+        "haslam_rating": "11",
         "seed": "2"
     },
-    "kenpom": {
-        "conference_record": "13-7",
-        "overall_record": "24-7",
-        "rating": 21,
-        "record": "11-0",
-        "sos": 76
-    },
     "kpi_sports": {
-        "kpi_ranking": 4
+        "kpi_ranking": 5
     },
     "massey_ratings": {
         "massey_rank": 13
@@ -170,36 +158,36 @@
         "coaches_rank": 15
     },
     "shot_quality": {
-        "sq_ranking": 26
+        "sq_ranking": 25
     },
     "sports_reference": {
-        "SRS_rank": 23
+        "SRS_rank": 24
     },
     "teamrankings": {
-        "make_tournament": "94.7%",
-        "most_likely_seed": "5",
+        "make_tournament": "94.0%",
+        "most_likely_seed": "6",
         "proj_conf_record": "12.1-7.9",
-        "projected_bid_pct": "94.7%",
+        "projected_bid_pct": "94.0%",
         "projected_losses": "7.9",
         "projected_wins": "23.1",
         "rank": "28"
     },
     "time": {
-        "iso_time": "2025-12-19T09:37:21.248621",
-        "time": "Dec. 19 at 9:37 AM"
+        "iso_time": "2025-12-20T13:54:29.729484",
+        "time": "Dec. 20 at 1:54 PM"
     },
     "trank": {
-        "torvik_NET": "22",
-        "trank": 22,
+        "torvik_NET": "21",
+        "trank": 21,
         "trank_make_tourney": 99.8,
         "trank_proj_record": "24-7 (13-7)",
         "trank_seed": "5",
-        "wab_rank": 7,
+        "wab_rank": 8,
         "wab_score": "+3.1"
     },
     "warrennolan": {
         "current_conf_record": "2-0",
-        "elo": "14",
+        "elo": "11",
         "nolan_net": "11",
         "quadrant_1": "1-0",
         "quadrant_2": "4-0",
